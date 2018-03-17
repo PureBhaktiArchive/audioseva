8af809b4cfbea9cb68739252d1873e2735b689cb
Anthony Serdyukov <anthony@edsd.com>
2018-03-17 17:49:48 +0700

    Replace select2 with selectize.

--- allot.js
+++ allot.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-  
+
   $.get("https://hook.integromat.com/41xuikkmbdo2ql56g6uwkxb656hp0zj4", null, null, "json")
     .then(function(data) {
       console.log(data);
-      $('#devotee').select2({
-        width: '100%',
+      $('#devotee').selectize({
         placeholder: 'Select a devotee',
-        data: data.results,
-        allowClear: true,
-        minimumResultsForSearch: 5,
+        options: data,
+        valueField: 'id',
+        labelField: 'name',
+        searchField: ['name', 'emailaddress'],
+        render: {
+          option: function(item, escape) {
+            return '<div>' +
+              '<span class="name">' + escape(item.name) + '</span>' +
+              '<span class="location">' + escape(item.location) + '</span>' +
+              '<span class="emailaddress">' + escape(item.emailaddress) + '</span>' +
+              '</div>';
+          }
+        }
       });
     });
   $("#files").multiselect({
