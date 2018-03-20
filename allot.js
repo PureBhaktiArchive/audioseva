17422bdac5316bdf428d98a2ef19b1cf198cd199
Ananta Krsna das <anantakrishna.kem@gmail.com>
2018-03-20 08:49:25 +0700

    Remove multiselect, add list switcher.

--- allot.js
+++ allot.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   $.get("https://hook.integromat.com/41xuikkmbdo2ql56g6uwkxb656hp0zj4", null, null, "json")
     .then(function(data) {
-      console.log(data);
       $('#devotee').selectize({
         placeholder: 'Select a devotee',
         options: data,
@@ -20,11 +19,36 @@ $(document).ready(function() {
         }
       });
     });
-  $("#files").multiselect({
-    numberDisplayed: 1,
-    closeOnSelect: false,
-    enableClickableOptGroups: true,
-    enableCollapsibleOptGroups: true,
+
+  $("input").change(function() {
+    $(".files").empty();
+    $.get({
+      url: "https://hook.integromat.com/kjdqf7lnvgisr4kpia4lxzsf3cw53nvn",
+      data: {
+        list: this.id
+      },
+      dataType: "json",
+      success: function(data) {
+        $.each(data, function(i, item) {
+          $(".files")
+            .append($('<div/>', {
+                class: 'checkbox'
+              })
+              .append($('<label/>')
+                .append($('<input/>', {
+                  'type': 'checkbox',
+                  value: item.index
+                }))
+                .append($('<strong>', {
+                  text: item.index + " "
+                }))
+                .append($('<code>', {
+                  text: item.filename
+                }))
+              ));
+        });
+      }
+    });
   });
 
 });
