789ea1de6d7eeb3a94468bfc7d4876b6e379255b
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2018-07-13 14:18:55 +0700

    Listen Page: remove OSF link.

--- sites/all/modules/listen_page/src/Controller/ListenController.php
+++ sites/all/modules/listen_page/src/Controller/ListenController.php
@@ -22,20 +22,10 @@ class ListenController extends ControllerBase {
     $file_name = str_replace('ML2-', '', $file_name); // ML2 prefix is artificial, files are without it.
     $folder = str_replace('ML1', 'Hindi', $list);
 
-    $form_link = \Drupal\Core\Url::fromUri("base:/online-submission", [
-      'query' => [
-        'tape_number' => $base_name,
-        'name' => \Drupal::request()->query->get('name'),
-        'email_address' => \Drupal::request()->query->get('email_address'),
-        'direct_input' => 'no',
-      ],
-    ]);
-
     return [
         '#title' => "Audio File Name: $base_name",
         '#theme' => 'listen_page',
         '#audio_src' => "https://vraja.info/All_mp3/newcapture/$folder/$file_name.mp3",
-        '#form_link' => $form_link,
       ];
   }
 }
