a0e8653e95d95a9111682bda7cb8fb977d6e1ad6
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2018-07-15 06:54:14 +0700

    Listen Page: fix issues.

--- sites/all/modules/listen_page/src/Controller/ListenController.php
+++ sites/all/modules/listen_page/src/Controller/ListenController.php
@@ -15,17 +15,18 @@ use Drupal\Core\Controller\ControllerBase;
 class ListenController extends ControllerBase {
 
   public function page($file_name) {
-    if (!preg_match('/^(\w+)-.+$/', $file_name, $matches))
-      die();
+    if (preg_match('/^(\w+)-.+$/', $file_name, $matches))
+      $folder = $matches[1];
+    else {
+      $folder = "Hindi";
+    }
 
-    $list = $matches[1];
-    $file_name = str_replace('ML2-', '', $file_name); // ML2 prefix is artificial, files are without it.
-    $folder = str_replace('ML1', 'Hindi', $list);
+    $real_file_name = str_replace('ML2-', '', $file_name); // ML2 prefix is artificial, files are without it.
 
     return [
-        '#title' => "Audio File Name: $base_name",
+        '#title' => "Audio File Name: $file_name",
         '#theme' => 'listen_page',
-        '#audio_src' => "https://vraja.info/All_mp3/newcapture/$folder/$file_name.mp3",
+        '#audio_src' => "https://vraja.info/All_mp3/newcapture/$folder/$real_file_name.mp3",
       ];
   }
 }
