b1a6caa7bb528755268ade5c42de738e8803eb5a
Ananta Krsna das <anantakrishna.kem@gmail.com>
2018-07-28 04:47:51 +0700

    Add Bootstrap, switch to SCSS.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "../node_modules/bootstrap/scss/bootstrap.scss";
+
 Vue.config.productionTip = false;
 
 new Vue({
