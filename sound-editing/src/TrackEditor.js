88626410ec563ae3dc175d04a4bcbc458e3e6f13
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2018-08-03 10:04:21 +0700

    TE: Add comment to incorrect files and move to Processed folder.

--- sound-editing/src/TrackEditor.js
+++ sound-editing/src/TrackEditor.js
@@ -3,7 +3,6 @@
  */
 
 import { Devotee } from './Devotee';
-import { DriveUtils } from './DriveUtils';
 
 const FLAC_MIME_TYPE = 'audio/flac';
 const FILE_NAME_PATTERN = /^(\w+-\d+-\d+)\.flac$/; // ML1-030-1.flac
@@ -23,11 +22,14 @@ export class TrackEditor extends Devotee {
       // Checking file name pattern
       const nameMatch = file.getName().match(FILE_NAME_PATTERN);
       if (!nameMatch) {
-        console.warn(
-          'File “%s” does not match the naming convention, moving to incorrect.',
-          file.getName()
+        console.warn('File “%s” does not match the naming convention.', file.getName());
+        Drive.Comments.insert(
+          { content: 'File does not match the naming convention.' },
+          file.getId()
         );
-        DriveUtils.moveFile(file, this.incorrectFolder);
+
+        this.processedFolder.addFile(file);
+        this.uploadsFolder.removeFile(file);
         continue;
       }
 
@@ -36,8 +38,10 @@ export class TrackEditor extends Devotee {
       // Finding corresponding task
       const task = this.tasksTable.getItemById(baseName);
       if (!task) {
-        console.warn('%s does not match any task, moving to incorrect.', file.getName());
-        DriveUtils.moveFile(file, this.incorrectFolder);
+        console.warn('File “%s” does not match any task, moving to incorrect.', file.getName());
+        Drive.Comments.insert({ content: 'Task is not found in your TE Doc.' }, file.getId());
+        this.processedFolder.addFile(file);
+        this.uploadsFolder.removeFile(file);
         continue;
       }
 
@@ -47,7 +51,9 @@ export class TrackEditor extends Devotee {
       // Making a copy and saving the file for further processing
       console.log('Copying %s into Edited folder.', file.getName());
       const copiedFile = file.makeCopy(this.editedFolder);
-      DriveUtils.moveFile(file, this.processedFolder);
+
+      this.processedFolder.addFile(file);
+      this.uploadsFolder.removeFile(file);
 
       // Saving file link
       task.setFieldValue('Edited File Link', copiedFile.getUrl());
