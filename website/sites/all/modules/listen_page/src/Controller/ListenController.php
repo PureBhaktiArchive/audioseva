c69d0603bfa76ddb266117b30cc76d95644d8c4d
Ananta Krsna das <anantakrishna.kem@gmail.com>
2018-10-11 11:15:32 +0700

    Route SHA and UMA lists to the Ghanasyama cloud from the listening page.

--- website/sites/all/modules/listen_page/src/Controller/ListenController.php
+++ website/sites/all/modules/listen_page/src/Controller/ListenController.php
@@ -14,7 +14,7 @@ use Drupal\Core\Controller\ControllerBase;
 
 class ListenController extends ControllerBase {
 
-  public function page($file_name) {
+  public function page_old($file_name) {
     if (preg_match('/^(\w+)-.+$/', $file_name, $matches))
       $folder = $matches[1];
     else {
@@ -27,6 +27,29 @@ class ListenController extends ControllerBase {
         '#title' => "Audio File Name: $file_name",
         '#theme' => 'listen_page',
         '#audio_src' => "https://vraja.info/All_mp3/newcapture/$folder/$real_file_name.mp3",
+      ];
+  }
+
+  private function audio_url($file_name) {
+    $list = preg_match('/^(\w+)-.+$/', $file_name, $matches) ? $matches[1] : "ML1";
+
+    $real_file_name = rawurldecode(str_replace('ML2-', '', $file_name)); // ML2 prefix is artificial, files are without it.
+
+    switch ($list) {
+      case "UMA":
+      case "SHA":
+        return "https://storage.googleapis.com/audio-seva/mp3/$list/$file_name.mp3";
+      default:
+        $folder = $list === "ML1" ? "Hindi" : $list;
+        return "https://vraja.info/All_mp3/newcapture/$folder/$real_file_name.mp3";
+    }
+  }
+
+  public function page($file_name) {
+    return [
+        '#title' => "Audio File Name: $file_name",
+        '#theme' => 'listen_page',
+        '#audio_src' => $this->audio_url($file_name),
       ];
   }
 }
