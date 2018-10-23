cdd92963f8a6c66fd10c4709b27b45f021228af0
Matthew Bernheimer <Krutonslol@gmail.com>
2018-10-23 14:18:33 -0400

    Update README with Firebase database url
    The env variable for the database url has been renamed to follow the
    same naming conventions as other Firebase env variables

--- frontend/src/firebaseApp.js
+++ frontend/src/firebaseApp.js
@@ -8,7 +8,7 @@ import "firebase/auth";
 export default firebase.initializeApp({
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FB_DATABASE_URL,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
   storageBucket: process.env.FB_STORAGE_BUCKET,
   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
 });
