7ee0a4a2f862086e19f4bdb493a445435d874624
Ahmad ElBatanouni <egyptstyles1@gmail.com>
2018-10-24 19:23:56 +0200

    Use sendInBlue API to contact the volunteer - Update the files as "Given"

--- functions/helpers.js
+++ functions/helpers.js
@@ -1,4 +1,3 @@
-
 let checkValidity = filePath => (filePath.startsWith("mp3/") && filePath.endsWith(".mp3"))
 
 let createRefString = filePath => {
@@ -26,4 +25,38 @@ exports.storeFileNameToDB = (filePath, db) => {
             return 1;
         }).catch(err => console.log(err));
     }
+}
+
+exports.removeFileNameFromDB = (db, dbPath) => {
+    let ref = db.ref(dbPath);
+    ref.remove()
+        .then(() => console.log("Deleted."))
+        .catch(error => console.log(error));
+}
+
+
+exports.updateFile = (db, ref, payload, newDocKey) => {
+    return ref.child("status").once('value')
+    .then(snapshot => {
+        if(snapshot.exists())
+            ref.update(payload, err => {
+                if(!err)
+                    db.ref(`/sqr/allotments/${newDocKey}`).update({ filesAlloted: true });
+            });
+        return 1;
+    }).catch(err => console.log(err));    
+}
+
+exports.sendEmail = (emailAddress, SibApiV3Sdk) => {
+    let apiInstance = new SibApiV3Sdk.SMTPApi();
+    let templateId = 2;// temporarily until we know where this will be stored
+
+    let sendEmail = new SibApiV3Sdk.SendEmail([emailAddress]);
+
+    apiInstance.sendTemplate(templateId, sendEmail).then(function(data) {
+        console.log('Message sent successfully. Returned data: ' + JSON.stringify(data));
+        return 1;
+    }, function(error) {
+        console.error(error);
+    }).catch(err => console.log(err));
 }
--- functions/index.js
+++ functions/index.js
@@ -8,15 +8,26 @@ admin.initializeApp({
     storageBucket: functions.config().audioseva.storage_bucket
 });
 
+
+// for contacting the sendinblue API
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+
+const sendInBlueSecretKey = functions.config().audioseva.send_in_blue.key;
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = sendInBlueSecretKey;
+
 const bucket = admin.storage().bucket();
 const db = admin.database();
 
+
+
 //Functions
 const importMp3IntoSQR = require('./src/importMp3IntoSQR');
-const importCurrentMP3IntoSQR = require('./src/importCurrentMP3IntoSQR');
+const syncStorageToDB = require('./src/syncStorageToDB');
+const allotments = require('./src/allotments');
 
 // Helper Functions
-// 1. storeFileNameToDB( filePath, db_object )
 const helpers = require('./helpers');
 
 
@@ -24,8 +35,28 @@ exports.importMP3IntoSQR = functions.storage.object().onFinalize( object => {
     return importMp3IntoSQR.handleNewUploads(object, db, helpers.storeFileNameToDB);
 });
 
-
-exports.importCurrentMP3IntoSQR = functions.https.onRequest((req, res) => {
-    return importCurrentMP3IntoSQR.handleCurrentlyUploadedFiles(res, bucket, db, helpers.storeFileNameToDB);
+exports.syncStorageToDB = functions.https.onRequest((req, res) => {
+    syncStorageToDB.handleCurrentlyUploadedFiles(bucket, db, helpers.storeFileNameToDB);
+    syncStorageToDB.removeNonExistingMp3DBEntries(bucket, db, helpers.removeFileNameFromDB);
+    return res.send(`Started Execution, the process is now Running in the background`);
 });
 
+///////////////////////////////////////////////////////////////////////////////////////////
+
+exports.new_allotment = functions.database.ref('/sqr/allotments/{allotment_id}')
+    .onCreate((snapshot, context) => {
+        return allotments.UpdateFilesOnNewAllotment(
+                snapshot, 
+                db,
+                helpers.updateFile
+            );
+    });
+
+exports.sendEmailOnFileAllotment = functions.database.ref('/sqr/allotments/{allotment_id}')
+    .onUpdate((change, context) => {
+        const old = change.before.val();
+        const _new = change.after.val();        
+        
+        allotments.sendEmailOnFileAllotment(old, _new, change.after, SibApiV3Sdk, helpers.sendEmail)
+        return 1;
+    });
