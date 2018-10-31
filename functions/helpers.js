6e0639e461f6a931d60d7a7733949e8548d6b30c
Ahmad ElBatanouni <egyptstyles1@gmail.com>
2018-10-31 12:44:04 +0200

    add function to handle allotment processing & submission processing

--- functions/helpers.js
+++ functions/helpers.js
@@ -1,34 +1,16 @@
+const functions = require('firebase-functions');
+let SibApiV3Sdk = require('sib-api-v3-sdk');
+const sendInBlueSecretKey = functions.config().audioseva.send_in_blue.key;
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = sendInBlueSecretKey;
 
-let checkValidity = filePath => (filePath.startsWith("mp3/") && filePath.endsWith(".mp3"))
 
-let createRefString = filePath => {
-    const parts = filePath.split('/');
-        
-    const list = parts[1];
-    const mp3 = parts[2];
-    let file_name = mp3.slice(0, -4);
 
-    return `/sqr/files/${list}/${file_name}`;
-}
+exports.checkValidMP3 = filePath => (filePath.startsWith("mp3/") && filePath.endsWith(".mp3"))
 
-exports.storeFileNameToDB = (filePath, db) => {
-    if(checkValidity(filePath)) {
-        let ref = db.ref( createRefString(filePath) );
 
-        //checking if the file already exists in the RT db
-        ref.child("status").once('value')
-        .then(snapshot => {
-            if(!snapshot.exists())
-                ref.set({status: "Spare"});
-            else
-                console.log("Existing");
-
-            return 1;
-        }).catch(err => console.log(err));
-    }
-}
-
-exports.removeFileNameFromDB = (db, dbPath) => {
+exports.removeFromDB = (db, dbPath) => {
     let ref = db.ref(dbPath);
     ref.remove()
         .then(() => console.log("Deleted."))
@@ -36,3 +18,20 @@ exports.removeFileNameFromDB = (db, dbPath) => {
 }
 
 
+exports.sendEmail = (to, bcc, templateId, params) => {
+    let apiInstance = new SibApiV3Sdk.SMTPApi();
+    sendEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+    sendEmail = {
+        to: [{ email: to }], // add a name prop if it fails to send
+        bcc,
+        templateId,
+        params
+    };
+
+    apiInstance.sendTransacEmail(sendEmail).then(function(data) {
+        console.log('API called successfully. Returned data: ');
+        console.log(data);
+        return 1;
+    }, err => console.log(err)).catch(err => console.log(err));
+}
--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,21 @@
 
 const functions = require('firebase-functions');
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
-admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: functions.config().audioseva.database_url,
-    storageBucket: functions.config().audioseva.storage_bucket
-});
+try {
+    admin.initializeApp({
+        credential: admin.credential.applicationDefault(),
+        databaseURL: functions.config().audioseva.database_url,
+        storageBucket: functions.config().audioseva.storage_bucket
+    });
+} catch (err) { console.log(err) }
 
-const bucket = admin.storage().bucket();
-const db = admin.database();
+//Loading the Functions from their modules
+const glob = require('glob');
+const functionFiles = glob.sync('./src/*.functions.js', { cwd: __dirname });
 
-//Functions
-const importMp3IntoSQR = require('./src/importMp3IntoSQR');
-const syncStorageToDB = require('./src/syncStorageToDB');
-
-// Helper Functions
-// 1. storeFileNameToDB( filePath, db_object )
-const helpers = require('./helpers');
-
-
-exports.importMP3IntoSQR = functions.storage.object().onFinalize( object => {
-    return importMp3IntoSQR.handleNewUploads(object, db, helpers.storeFileNameToDB);
-});
-
-
-exports.syncStorageToDB = functions.https.onRequest((req, res) => {
-    syncStorageToDB.handleCurrentlyUploadedFiles(bucket, db, helpers.storeFileNameToDB);
-    syncStorageToDB.removeNonExistingMp3DBEntries(bucket, db, helpers.removeFileNameFromDB);
-    return res.send(`Started Execution, the process is now Running in the background`);
+functionFiles.forEach((file, index) => {
+    const filename = file.split('/').pop().slice(0, -13); // remove the trailing ".functions.js"
+    exports[filename] = require(file);
 });
 
