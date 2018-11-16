f78c688f4b9cba8d07349c9046c9fff6ff43621e
Matthew Bernheimer <Krutonslol@gmail.com>
2018-11-16 03:40:15 -0500

    Update table data styles

--- frontend/src/components/SE/SoundIssuesList.vue
+++ frontend/src/components/SE/SoundIssuesList.vue
@@ -1,13 +1,14 @@
 <template>
   <ul>
-    <li v-for="(value, key, index) in item.soundIssues" :key="index">
-      {{ `${value.beginning}-${value.ending}: ${value.type}: ${value.description || ""}` }}
+    <li :style="{ whiteSpace: 'nowrap' }" v-for="(value, key, index) in item.soundIssues" :key="index">
+      {{ `${formatSeconds(value.beginning)}-${formatSeconds(value.ending)} ${value.type}, ${value.description || ""}` }}
     </li>
   </ul>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import moment from "moment";
 
 @Component({
   name: "SoundIssuesList"
@@ -15,6 +16,12 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class SoundIssuesList extends Vue {
   @Prop() item!: any;
   @Prop() value!: string;
+
+  formatSeconds(seconds: number) {
+    return moment
+      .utc(moment.duration(seconds, "seconds").asMilliseconds())
+      .format("mm:ss");
+  }
 }
 </script>
 
