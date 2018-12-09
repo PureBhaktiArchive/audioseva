55dc57267f72b2e4e46b0a39b7411ad009b6075e
Nadir Abbas <38838675+nadirabbas@users.noreply.github.com>
2018-12-09 19:19:49 +0500

    Add Users Import From Spreadsheets to Firebase

---
+++ functions/src/modules/users.functions.ts
@@ -0,0 +1,212 @@
+/////////////////////////////////////////////////
+//          Import Registered Users from a Spreadsheet(Http Triggered)
+//
+//      1. Parses a google spreadsheet
+//      2. Looks for two sheet --> Registrations
+//      3. Loads their data into the equivalent Firebase database paths
+/////////////////////////////////////////////////
+
+// Imports
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { google } from 'googleapis';
+
+const db = admin.database();
+
+export const importUserRegistrationData = functions.https.onRequest(
+    async (req, res) => {
+        // Default 400 Bad Request Response
+        const badRequest = {
+            status: 400,
+            res: {
+                message:
+                    'Please define the sheet id, e.g sheet={SpreadSheet ID}',
+                status: false
+            }
+        };
+        // Grab the SpreadSheet ID from the Query Parameters
+        const spreadSheetId = req.query.sheet ? req.query.sheet : null;
+        if (!spreadSheetId) {
+            res.status(badRequest.status).send(badRequest.res);
+        } else {
+            performUserSpreadSheetImport(spreadSheetId).then(response => {
+                res.send(response['res']);
+            });
+        }
+    }
+);
+
+// Helper functions importUserRegistrationData
+const performUserSpreadSheetImport = sheet => {
+    // Variables to populate laters (used for sending back a detailed response to the user)
+    let usersAdded = [];
+    let usersAlreadyExist = [];
+    return new Promise(async (resolve, reject) => {
+        const auth = await google.auth.getClient({
+            scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+        });
+
+        const spreadsheet = google.sheets({ version: 'v4', auth });
+        spreadsheet.spreadsheets.values.get(
+            { spreadsheetId: sheet, range: 'Registrations' },
+            (err, response) => {
+                if (err) {
+                    reject({
+                        status: 400,
+                        res: {
+                            status: false,
+                            message: 'Sheet not found',
+                            code: 'INVALID_SHEET_ID'
+                        }
+                    });
+                } else {
+                    var cache = [];
+                    var resp = JSON.parse(
+                        JSON.stringify(response, function(key, value) {
+                            if (typeof value === 'object' && value !== null) {
+                                if (cache.indexOf(value) !== -1) {
+                                    // Duplicate reference found
+                                    try {
+                                        // If this value does not reference a parent it can be deduped
+                                        return JSON.parse(
+                                            JSON.stringify(value)
+                                        );
+                                    } catch (error) {
+                                        // discard key if value cannot be deduped
+                                        return;
+                                    }
+                                }
+                                // Store value in our collection
+                                cache.push(value);
+                            }
+                            return value;
+                        })
+                    );
+
+                    // Take Actual Values and Field Names apart
+                    let sheetFields = resp.data.values[0];
+                    let sheetRows = resp.data.values.slice(1);
+
+                    // Filter out users (Only Allow the user Custom claim CR with value YES)
+                    const users = sheetRows.filter(
+                        user =>
+                            user[5] != '' &&
+                            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                                user[5]
+                            )
+                    );
+                    // Store the users that were neglected because of invalid emails
+                    const usersNeglected = sheetRows.filter(
+                        user =>
+                            user[5] == '' ||
+                            !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                                user[5]
+                            )
+                    );
+
+                    if (!users) {
+                        resolve({
+                            status: 200,
+                            res: {
+                                status: true,
+                                message:
+                                    'No users found in the available users or fields such as email are invalid',
+                                usersNeglected
+                            }
+                        });
+                    } else {
+                        // Prepare to insert data in Firebase
+                        users.map(async (user, index) => {
+                            let newUser = {};
+
+                            // Preparing the values for DB insertion
+                            // Convert the String Date to UNIX Timestamp newCoordniator[2] is the Date's String Format
+                            newUser['timestamp'] = user[2]
+                                ? new Date(user[2]).getTime()
+                                : Date.now();
+                            // Convert Languages String to Array Type (Split by Commas)
+                            newUser['languages'] = {};
+                            const langArray = user[8]
+                                ? user[8].replace(' ', '').split(',')
+                                : [];
+                            if (langArray) {
+                                langArray.map(langName => {
+                                    newUser['languages'][langName] = true;
+                                });
+                            }
+                            // Map Details Column to notes
+                            newUser['notes'] = user[0];
+
+                            // Merge roles colums in to an array of booleans
+                            newUser['roles'] = {};
+                            const roleFieldArray = sheetFields.slice(10, 15);
+                            const roleArray = user.slice(10, 15);
+                            if (roleFieldArray) {
+                                roleFieldArray.map((roleFieldName, index) => {
+                                    if (roleArray[index] == 'Yes')
+                                        newUser['roles'][roleFieldName] = true;
+                                });
+                            }
+
+                            // Add other spreadsheet field values to newUser
+                            newUser = {
+                                ...newUser,
+                                recommendedBy: user[18],
+                                services: user[9],
+                                isAvailableOnWhatsApp:
+                                    user[7] == 'Yes' ? true : false,
+                                experience: user[16],
+                                influencer: user[17],
+                                phoneNumber: user[6],
+                                emailAddress: user[5],
+                                location: user[4],
+                                name: user[3],
+                                status: user[1],
+                                created: Date.now()
+                            };
+
+                            const response = () => {
+                                if (index == users.length - 1) {
+                                    return {
+                                        status: true,
+                                        message: `Added ${
+                                            usersAdded.length
+                                        } users into Firebase and neglected ${usersNeglected.length +
+                                            usersAlreadyExist.length} users`,
+                                        usersNeglected,
+                                        usersAdded,
+                                        usersAlreadyExist
+                                    };
+                                } else {
+                                    return {};
+                                }
+                            };
+                            // Insert into DB (neglect the users that already exist)
+                            await db
+                                .ref(`/users`)
+                                .orderByChild('emailAddress')
+                                .equalTo(newUser['emailAddress'])
+                                .once('value', snapshot => {
+                                    if (!snapshot.exists()) {
+                                        db.ref(`/users`).push(newUser, err => {
+                                            if (!err) {
+                                                usersAdded.push(newUser);
+                                                if (index == users.length - 1) {
+                                                    resolve({
+                                                        res: response()
+                                                    });
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        usersAlreadyExist.push(newUser);
+                                        resolve({ res: response() });
+                                    }
+                                });
+                        });
+                    }
+                }
+            }
+        );
+    });
+};
