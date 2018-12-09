0ebb1d39a709fef58f863020bd5e9ad03ff1b1cb
Nadir Abbas <38838675+nadirabbas@users.noreply.github.com>
2018-12-09 19:42:29 +0500

    Add Users Import from Spreadsheets to Firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as  admin from 'firebase-admin';
+import * as admin from 'firebase-admin';
 import 'firebase-functions';
 const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
@@ -8,24 +8,26 @@ try {
         databaseURL: adminConfig.databaseURL,
         storageBucket: adminConfig.storageBucket
     });
-} catch (err) { console.log(err) }
-
+} catch (err) {
+    console.log(err);
+}
 
 /*********************************************
-**
-**  ** Loads the Functions from their modules and then exports them **
-**      The resulting exports object will have multiple inner objects with each having 
-**          KEY = file name
-**          VALUE = all the functions in the loaded file name
-**
-**      Example --> exports = { SQR: { updateFilesOnNewAllotment: [Function], ... } }
-**      On Cloud Fucntions this will appear as SQR-updateFilesOnNewAllotment
-**
-**********************************************/
+ **
+ **  ** Loads the Functions from their modules and then exports them **
+ **      The resulting exports object will have multiple inner objects with each having
+ **          KEY = file name
+ **          VALUE = all the functions in the loaded file name
+ **
+ **      Example --> exports = { SQR: { updateFilesOnNewAllotment: [Function], ... } }
+ **      On Cloud Fucntions this will appear as SQR-updateFilesOnNewAllotment
+ **
+ **********************************************/
 import * as SQR from './modules/SQR.functions';
 import * as ReportingSync from './modules/reporting.sync.functions';
 import * as Email from './modules/email.functions';
 import * as SE from './modules/SE.functions';
-import * as Authorization from "./modules/Authorization.functions";
+import * as Authorization from './modules/Authorization.functions';
+import * as Users from './modules/users.functions';
 
-export { SQR, ReportingSync, Email, Authorization, SE };
+export { SQR, ReportingSync, Email, Authorization, SE, Users };
