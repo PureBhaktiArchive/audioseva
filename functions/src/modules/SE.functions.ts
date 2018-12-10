d327f496a5dddc0566cc1931db56bd73d685e288
Ahmad ElBatanouni <egyptstyles1@gmail.com>
2018-12-10 12:42:50 +0200

    Adaptation of the new requirements

--- functions/src/modules/SE.functions.ts
+++ functions/src/modules/SE.functions.ts
@@ -4,6 +4,8 @@ import * as  admin from 'firebase-admin';
 import uniqid from 'uniqid';
 
 const db = admin.database();
+const bucket = admin.storage().bucket();
+
 
 /////////////////////////////////////////////////
 //          processNewAllotment (DB create Trigger)
@@ -76,4 +78,95 @@ export const processNewAllotment = functions.database
         }
     });
     return 1;
+});
+
+
+
+
+/////////////////////////////////////////////////
+//
+//          UploadProcessing (Storage Trigger)
+//      1. Send a notification to the coordinator
+//      2. Update the Task
+//
+/////////////////////////////////////////////////
+
+export const uploadProcessing = functions.storage.object()
+.onFinalize(async object => {    
+
+    const filePath = object.name;
+
+
+    if (!filePath.startsWith('sound-editing/restored/uploads'))
+        return -1;
+    
+    
+    const filePathRegex = /sound-editing\/restored\/uploads\/(\w+)\/(\w+)\/(\w+\/)*(([\w\d]+)-\d+-?\d+?)\.flac/;
+    
+    const match = filePathRegex.exec(filePath);
+    if (!match) {
+        console.log("Wrong Path -- File will be deleted.");
+        bucket.file(object.name).delete();
+        return -1;
+    }
+
+    const uploadCode = match[1], 
+            timestamp = match[2],
+            list = match[match.length - 1], 
+            taskId = match[match.length - 2];
+    
+
+    let supposedAssignee = (await db.ref(`/users`).orderByChild('uploadCode').equalTo(uploadCode).once('value')).val();
+    
+
+    
+    if (!supposedAssignee.exists()) { 
+        console.log("Wrong Upload code -- File will be deleted.");
+        bucket.file(object.name).delete();
+        return -1;
+    }
+
+    // 1. Send a notification to the coordinator
+    const taskRef = await db.ref(`/sound-editing/tasks/${list}/${taskId}`).once('value');
+
+    if (!taskRef.exists()) {
+        console.log("Task does not exist -- File will be deleted.");
+        bucket.file(object.name).delete();
+        return -1;
+    }
+
+    const task = taskRef.val();
+
+    if (['Revise', 'Spare'].indexOf(task.restoration.status) < 0) {
+        console.log("Incorrect task status (only [Revise OR Spare] is allowed here) -- File will be deleted.");
+        bucket.file(object.name).delete();
+        return -1;
+    }
+
+    const coordinator = functions.config().coordinator; // TO
+    const replyTo = task.restoration.assignee.emailAddress;
+
+    db.ref(`/email/notifications`).push({
+        template: 'se-upload',
+        to: coordinator.emailAddress,
+        replyTo,
+        params: { task }
+    });
+
+    // 2. Upload a copy of the file to `/sound-editing/restored/$list/$taskId.flac`
+    bucket.file(object.name)
+        .copy(bucket.file(`sound-editing/restored/${list}/${taskId}.flac`));
+
+
+    // 3. Update the Task
+    let taskRestorationUpdate = {
+        status: 'In Review',
+        timestampLastVersion: admin.database.ServerValue.TIMESTAMP
+    };
+
+    if (!task.restoration.timestampFirstVersion)
+        taskRestorationUpdate['timestampFirstVersion'] = admin.database.ServerValue.TIMESTAMP;
+
+
+    return taskRef.ref.child('restoration').update(taskRestorationUpdate);    
 });
