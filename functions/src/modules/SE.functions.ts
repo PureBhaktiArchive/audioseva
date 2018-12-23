38311203545ef76f3f062bd00ff93ac20dd5b7d0
Ahmad ElBatanouni <egyptstyles1@gmail.com>
2018-12-23 08:57:18 +0200

    Import Sheets with required fields only & ensure data passes validity checks & sort chunks by start time & report overlapping chunk times & ensure data is never overwritten in db

--- functions/src/modules/SE.functions.ts
+++ functions/src/modules/SE.functions.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-
+import { google } from "googleapis";
+const lodash = require('lodash');
 import uniqid from 'uniqid';
 
 const db = admin.database();
@@ -88,3 +89,224 @@ export const processNewAllotment = functions.database
     });
     return 1;
   });
+
+
+const timeToMins = (time) => {
+    let hours = time.split(':')[0];
+    let mins = time.split(':')[1];
+    return (+hours * 60) + (+mins);
+}
+
+export const importSpreadSheetData = functions.runWith({
+  timeoutSeconds: 300,
+  memory: '1GB'
+}).https.onRequest( async (req, res) => {
+  const auth = await google.auth.getClient({
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+
+  const spreadsheetId = functions.config().sound_editing.spreadsheet_id;
+
+  const currentSheets = await sheets.spreadsheets.get({
+      spreadsheetId
+  });
+
+  ////////////////////////
+  // Building an index
+  ////////////////////////
+
+  const sheetTitles = [];
+  let columnsIndex = {}; // { ColumnName: column_index }
+
+  for (let i = 0; i < currentSheets.data.sheets.length; i++) {
+      let { title } = currentSheets.data.sheets[i].properties;
+      const result = await sheets.spreadsheets.values.get({
+          spreadsheetId,
+          range: `${title}!1:1` // all the cells in the first row `Column headers`
+      });
+
+      if (!result.data.values || result.data.values.length === 0) {
+          console.log(`No data found in sheet: ${title}`);
+          continue;
+      }
+  
+      // Filtering the sheets
+      const row = result.data.values[0] 
+      if (row.indexOf('Beginning') > -1 && row.indexOf('Ending') > -1) {
+          sheetTitles.push(title);
+          row.forEach((col, i) => {
+              columnsIndex[col.replace(/ /g, '')] = i;
+          });
+      }
+  }
+
+
+  let resolutions = ['ok', 'drop', 'duplicate', 'on_hold', 'reallot', 'repeat', 'derivative'];
+  let resolution, fidCheckRes;
+  let lastFile,
+      summary = { 
+          addedChunks: 0, 
+          emptyValues: 0, 
+          misplaced: 0, 
+          noResolution: 0, 
+          overlappingChunks: [] 
+      };
+
+  for (let i = 0; i < sheetTitles.length; i++) {
+      const result = await sheets
+          .spreadsheets.values.get({
+              spreadsheetId,
+              range: `${sheetTitles[i]}!A2:AZ1000`
+          });
+  
+      const rows = result.data.values;        
+
+      lastFile = { 
+          file_name: null,
+          chunks: [], 
+          skip: false, 
+          track: null 
+      };
+
+      for (const row of rows) {
+          let skip = false;
+
+          // Misplaced audiofile entry
+          if (row[columnsIndex['AudioFileName']].split('-')[0] != sheetTitles[i]) {
+              summary.misplaced++;
+              continue;
+          }
+
+
+          resolution = row[columnsIndex['Resolution']];
+          fidCheckRes = row[columnsIndex['FidelityCheckResolution']];
+
+          if (resolution && fidCheckRes) {
+          
+              resolution = resolution.toLowerCase().replace(' ', '_');
+              fidCheckRes = fidCheckRes.toLowerCase().replace(' ', '_');
+
+              if (resolutions.indexOf(resolution) < 0 || resolutions.indexOf(fidCheckRes) < 0) {
+                  summary.noResolution++;
+                  skip = true;
+                  // Didn't use `continue` here cause we want to skip all of the chunks
+                  // not just this one
+              }
+          } else {
+              summary.noResolution++;
+              skip = true;
+          }
+
+
+          if (row[columnsIndex['Beginning']] === '' || row[columnsIndex['Ending']] === '') {
+              summary.emptyValues++;
+              continue;
+          }
+
+          let AudioFileName = row[columnsIndex['AudioFileName']]
+
+
+          let trackNameRegex = /\w+-\d+(.*)/;            
+          let track = trackNameRegex.exec(AudioFileName)[1];
+
+
+          if (lastFile.file_name === AudioFileName) {
+              if (!skip)
+                  lastFile.chunks.push({
+                      audioFileName: AudioFileName,
+                      beginning: row[columnsIndex['Beginning']],
+                      ending: row[columnsIndex['Ending']],
+                      continuationFrom: row[columnsIndex['ContinuationFrom']]
+                  });
+          } else { // NEW FILE -- Save the previous Chunks and CLEAR
+              if (lastFile.file_name != null && !lastFile.skip) {
+                  summary.addedChunks++;
+
+
+                  let lastEndingTime = -1;
+                  for (let k = 0; k < lastFile.chunks.length; k++) {
+                      let chunk = lastFile.chunks[k];
+                      if (!chunk.continuationFrom)
+                          continue;
+                      //////////////////////////////////
+                      // Ensuring chunks do NOT overlap
+                      //////////////////////////////////
+                      if (timeToMins(chunk.beginning) < lastEndingTime && 
+                          timeToMins(chunk.beginning) != 0 && lastFile.track == track) {
+                              summary.overlappingChunks.push(lastFile);
+                              console.log('Overlapping chunks');
+                      }
+                  }
+
+
+                  ///////////////////////////////
+                  // Sorting by Beginning time
+                  ///////////////////////////////
+                  let chunks = lastFile.chunks;
+
+                  // `start` attribute is added to be used in sorting
+                  chunks.forEach(element => element.start = timeToMins(element.beginning));
+                  chunks = lodash.orderBy(chunks, ['start'], ['asc']);
+
+                  // getting rid of the added `start` attribute
+                  chunks.forEach(element => delete element.start);
+
+
+
+                  ////////////////////////////////////////////////////////
+                  // Filling `continuationFrom` only for the FIRST column
+                  ////////////////////////////////////////////////////////
+                  let continuationFrom = null;
+                  chunks.forEach(chunk => {
+                      if (!chunk.continuationFrom)
+                          return;
+                      
+                      continuationFrom = chunk.continuationFrom;
+                      delete chunk.continuationFrom;
+                  });
+
+                  if (continuationFrom)
+                      chunks[0].continuationFrom = continuationFrom;
+
+
+
+                  //////////////////////////////////////
+                  // Ensuring data is NEVER overwritten
+                  //////////////////////////////////////
+                  let ref = await db
+                      .ref(`/sound-editing/chunks/${sheetTitles[i]}/${lastFile.file_name}`)
+                      .once('value');
+
+                  if (!ref.exists())
+                      await db
+                          .ref(`/sound-editing/chunks/${sheetTitles[i]}/${lastFile.file_name}`)
+                          .update(lastFile.chunks);
+              }
+              
+              lastFile.file_name = row[columnsIndex['AudioFileName']];
+              lastFile.chunks = [{
+                  audioFileName: row[columnsIndex['AudioFileName']],
+                  beginning: row[columnsIndex['Beginning']],
+                  ending: row[columnsIndex['Ending']],
+              }];
+              lastFile.skip = skip;
+          }
+      }
+  }
+
+  //////////////////////////////////////
+  // Notifying the admin of the results
+  //////////////////////////////////////
+  await db.ref(`/email/notifications`).push({
+      template: 'importing-se-chunks',
+      to: functions.config().coordinator.email_address,
+      bcc: [{ email: functions.config().coordinator.email_address }],
+      params: {
+          summary
+      }
+  });
+
+  res.status(200).send(`Added chunks: ${summary.addedChunks}, Records with empty values: ${summary.emptyValues}, No resolution: ${summary.noResolution}`);
+});
