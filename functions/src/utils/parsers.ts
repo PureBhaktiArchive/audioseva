d7f30acb173f48eaf1aaae472ac7c558dc3310b0
adrienshen <adrienshen.dev@gmail.com>
2019-01-03 21:28:54 +0800

    Default no value is empty string

--- functions/src/utils/parsers.ts
+++ functions/src/utils/parsers.ts
@@ -8,7 +8,7 @@ export function spreadsheetDateFormat(date: number): string {
   return moment(date*1000).format("MM/DD/YYYY");
 }
 
-export function withDefault(text: string, missing: string = "-") {
+export function withDefault(text: string, missing: string = "") {
   return text ? text : missing;
 }
 
