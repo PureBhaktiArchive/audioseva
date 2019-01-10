3cb1fd250261d634a3e3b78a3af718c83253b383
Ahmad ElBatanouni <egyptstyles1@gmail.com>
2019-01-10 20:27:38 +0200

    Use `google sheets service` as requested & Import Sheets with required fields only & ensure data passes validity checks & sort chunks by start time & report overlapping chunk times & ensure data is never overwritten in db & use `warn` instead of `log` & remove unnecessary checks & use a `validateFileChunks` function to break down the code & restructure the `summary` object as requested

--- functions/src/modules/SE.functions.ts
+++ functions/src/modules/SE.functions.ts
@@ -3,6 +3,13 @@ import * as admin from 'firebase-admin';
 
 import uniqid from 'uniqid';
 
+const lodash = require('lodash');
+import * as moment from 'moment';
+import * as helpers from './../helpers';
+
+
+import { google } from "googleapis";
+import GoogleSheets from '../services/GoogleSheets';
 import { taskIdRegex } from "../helpers";
 
 const db = admin.database();
@@ -185,4 +192,303 @@ export const createTaskFromChunks = functions.database.ref(
     duration
   });
   return snapshot.val();
+});
+
+
+
+const validateSpreadsheetRow = (row, summary, sheetTitle) => {
+  let resolutions = ['ok', 'drop', 'duplicate', 'on hold', 'reallot', 'repeat', 'derivative'];
+  
+  let _import = false;
+  let rowValid = true;
+
+  // [CHECK #1] File name belongs to the list identified by the sheet name			 
+  if (helpers.extractListFromFilename(row['Audio File Name']) != sheetTitle) {
+    console.warn("File name belongs to a different list.");//edited				
+    summary.validityFailures.push({//edited
+      row,
+      issues: ['misplaced']
+    });
+    rowValid = false;
+  }
+
+  const resolution = row['Resolution'].toLowerCase();
+  const fidCheckRes = row['Fidelity Check Resolution'].toLowerCase();
+
+  // [CHECK #2] Both resolutions belong to the list of resolutions, case-insensitively.
+  if (resolutions.indexOf(resolution) < 0 || resolutions.indexOf(fidCheckRes) < 0) {
+    console.warn(`Audio file name: ${row['Audio File Name']} -- Error: Resolution, or Fidelity check resolution values isn't valid`);//edited
+    if (rowValid)
+      summary.validityFailures.push({
+        row,
+        issues: ['invalidResoultion']
+      });
+    else 
+      summary.validityFailures[summary.validityFailures.length - 1].issues.push('invalidResoultion');
+    rowValid = false;
+  }
+
+  // [CHECK #6] Beginning and Ending should be filled for rows with Resolution equal to `OK`.
+  if (row['Beginning'] !== '' || row['Ending'] !== '' && resolution === 'ok') 
+    _import = true;
+
+      
+  // `Beginning` value should be less than `Ending`.
+  if (moment.duration(row['Beginning']).asSeconds() > moment.duration(row['Ending']).asSeconds()) {
+    console.warn(`Audio file name: ${row['Audio File Name']} -- Error: "Beginning" value isn't less than "Ending".`);
+    if (rowValid)
+      summary.validityFailures.push({
+        row,
+        issues: ['BeginningGreaterThanEnding']
+      });
+    else 
+      summary.validityFailures[summary.validityFailures.length - 1].issues.push('BeginningGreaterThanEnding');
+  }
+  return _import;
+}
+
+
+////////////////////////////////
+//			validateFileChunks
+//					1. Performing a validity check that can be performed
+//						only after the chunks are grouped
+//					2. Reporting chunks that are not the first chunk of a file and having `continuationfrom`
+////////////////////////////////
+const validateFileChunks = (chunks, fileName, summary) => {
+  let chunksToImport = [];
+  ////////////////////////////////////////////
+  // [CHECK #5] Chunks of a single audio file 
+  // should not overlap each other in timing.
+  ////////////////////////////////////////////
+  let lastEndingTime = null;
+  chunks.forEach((chunk, i) => {								
+    // `beginning` of the CURRENT chunk should be GREATER than the `ending` of the LAST chunk
+    if (i !== 0 && chunk.beginning && lastEndingTime && chunk.beginning < lastEndingTime) {
+      console.warn(`Audio file name: ${fileName} -- Error: Overlapping chunks.`);
+      summary.validityFailures.push({
+        audioFileName: fileName,
+        row: chunk,
+        issues: ['overlappingChunks']
+      });
+    }
+
+
+    // setting the value of `lastEndingTime` to be used in the next iteration
+    if (chunk.ending)
+      lastEndingTime = chunk.ending;
+    else
+      lastEndingTime = null;
+
+
+    //////////////////////////////////////////////////
+    // [CHECK #4] `continuationFrom` column should
+    // be filled only for the first chunk of the track.
+    //////////////////////////////////////////////////
+    if (chunk.continuationFrom && i !== 0) 
+      summary.validityFailures.push({
+        row: chunk,
+        issues: ['misplacedContinuationFrom']
+      });
+    
+    
+    // importing only chunks with `_import` flag set to true
+    //	( the ones that passed all the validty checks )
+    if (chunk._import) {
+      delete chunk._import;
+      chunksToImport.push(chunk);
+    }
+  });
+  return chunksToImport;
+}
+
+
+
+export const importChunks = functions.runWith({
+  timeoutSeconds: 300,
+  memory: '2GB'
+}).pubsub.topic('daily-tick').onPublish(async (message) => {
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+  const spreadsheetId = functions.config().reporting.content.processing.spreadsheet_id;
+  let sheetsInfo = await helpers.buildSheetIndex(sheets, spreadsheetId);
+  
+  const validSheetsTitles = [];
+  sheetsInfo.forEach(sheet => {
+    // Skip sheets not having (`Beginning` & `Ending`) columns //edited
+    if (sheet.firstRow.indexOf('Beginning') >= 0 && sheet.firstRow.indexOf('Ending') >= 0)
+      validSheetsTitles.push(sheet.title);
+  });
+
+
+  let currentFile, // used to increment the chunks of an audiofile coming from multiple rows into a single object
+    summary = {
+      validityFailures: [],
+      addedChunks: 0
+    };
+
+
+  // `for` is used instead of `forEach` to get the updated value of `summary` 
+  // after all the rows are read and validated
+  // If `forEach` is used instead it requires its inner function to be `async` to allow for the 
+  //	other logic to use the `await` keyword ..
+  //	`forEach` doesn't wait in turn for its inner async functions to end, resulting the `summary`
+  //	object to stay the same as it was before executing the `forEach`
+
+  for(const sheetTitle of validSheetsTitles) {
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // [TEMPORARY] this is added because of the limitation of 1000 rows, 26 columns per request
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    if (sheetTitle === 'ML2')
+        continue;
+
+    const gsheets = new GoogleSheets(
+      spreadsheetId,
+      sheetTitle
+    );
+
+    const rows = await gsheets.getRows();    
+    
+    // Maps each audio file name with its chunks
+    //			EXAMPLE:
+    //					chunks = {
+    //							"BR-01A": [ chunk#1, chunk#2, .... ],
+    //							"BR-03A": [ chunk#1, chunk#2, .... ],
+    //					}
+    let chunks = {};
+    
+    currentFile = { 
+      file_name: null,
+      chunks: []
+    };
+
+    ////////////////////////////////////////
+    //	
+    //	1. loop through all the rows in the current sheet
+    //	2. Validate each row against the following checks:
+    //			A. Misplaced files
+    //			B. Non-existence of `Resolution` values in a row
+    //			C. Empty values in either `Beginning` or `Ending` attributes
+    //			D. `Beginning` being greater than `Ending`
+    //	3. Build the `chunks` index object with the relative chunks
+    //
+    ////////////////////////////////////////
+    for (const row of rows) {
+
+      let _import;
+
+      if (!row['Resolution'] || !row['Fidelity Check Resolution'])
+        _import = false;
+      else 
+        _import = validateSpreadsheetRow(row, summary, sheetTitle);
+          
+      const audioFileName = row['Audio File Name'];
+      const continuationFrom = row['Continuation From'];
+
+      const chunkToStore = {
+        beginning: moment.duration(row['Beginning']).asSeconds(),
+        ending: moment.duration(row['Ending']).asSeconds(),
+        continuationFrom,
+        contentReporting: {
+          date: row['Date'],
+          locatioon: row['Location'],
+          category: row['Category'],
+          topics: row['Topics'],
+          gurudevaTimings: row['Gurudeva Timings'],
+          otherSpeakers: row['Other Speakers'],
+          kirtan: row['Kirtan'],
+          abruptLecture: row['Abrupt Lecture'],
+          suggestedTitle: row['Suggested Title'],
+          languages: row['Languages'],
+          soundQualityRating: row['Sound Quality'],
+          soundIssues: row['Sound Issues'],
+          comments: row['Comments'],
+          submissionTimestamp: row['Timestamp'],
+          submissionSerial: row['Submission Serial'],
+        },
+        importTimestamp: admin.database.ServerValue.TIMESTAMP,
+        processingResolution: row['Resolution'],
+        _import
+      };
+
+          
+      // Same file name --> add newly read chunk (row) to the `currentFile` object
+      if (currentFile.file_name === audioFileName) 
+        currentFile.chunks.push(chunkToStore);
+      
+      // NEW FILE -->
+      //			Flush the read chunks into the corresponding file in the `chunks` index
+      //			& RESET the `currentFile` object
+      else {
+        if (currentFile.file_name != null) {
+          // if file object exists, add the chunks to it
+          if (chunks[currentFile.file_name]) 
+            chunks[currentFile.file_name].push(currentFile.chunks)
+
+          // if this is the first occurence of the file, set its value to the list of chunks 
+          else		
+            chunks[currentFile.file_name] = currentFile.chunks;
+        }
+
+        // Reset the `currentFile` object
+        currentFile.file_name = audioFileName;
+        currentFile.chunks = [chunkToStore];
+      }										
+    }
+    
+
+    
+
+    const fileNames = Object.keys(chunks);
+    for (let fileName of fileNames) {
+      let currentFilechunks = chunks[fileName];
+
+      // Sorting by `beginning` time
+      currentFilechunks = await lodash.orderBy(currentFilechunks, ['beginning'], ['asc']);
+
+      let chunksToImport = validateFileChunks(currentFilechunks, fileName, summary);
+
+      summary.addedChunks += chunksToImport.length;
+
+      //////////////////////////////////////
+      // Ensuring data is NEVER overwritten
+      //////////////////////////////////////
+      const dbRef = `/sound-editing/chunks/${sheetTitle}/${fileName.split(' ')[0]}`;
+      
+      const ref = await db.ref(dbRef).once('value');
+
+      if (!ref.exists())
+        await db.ref(dbRef).set(chunksToImport);
+      else {
+        const dbChunks = ref.val();
+        dbChunks.forEach((dbChunk, i) => {									 
+          // Instead of comparing every single attribute,
+          // the two chunk object are converted into strings and then compared
+          if (JSON.stringify(dbChunk) != JSON.stringify(currentFilechunks[i])) {								
+            console.warn(`Audio file name: ${fileName} -- Error: Modified data.`);
+            summary.validityFailures.push({																				
+              row: dbChunk,
+              currentFilechunks,
+              issues: ['modifiedData']
+            });
+            return;
+          }								 
+        });
+      }
+    }
+  }	 
+
+  
+  //////////////////////////////////////
+  // Notifying the coordinator of the results
+  //////////////////////////////////////
+  await db.ref(`/email/notifications`).push({
+      template: 'importing-se-chunks',
+      to: functions.config().coordinator.email_address,
+      params: summary
+  });		
 });
