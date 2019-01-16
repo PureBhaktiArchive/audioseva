823b1729d42be6bae2e4a3ba268f3fc2ed0d54aa
adrienshen <adrienshen.dev@gmail.com>
2019-01-16 18:04:30 +0800

    Change languages to boolean object

--- functions/src/modules/Users.functions.ts
+++ functions/src/modules/Users.functions.ts
@@ -38,7 +38,7 @@ enum Decision {
 /**
  * 1. Scan the Registrations Google Sheet and exports any new users to database
  * 2. Do not import if user email already exists
- * 
+ *
  */
 export const importUserRegistrationData = functions.https.onRequest(
   async (req: functions.Request, res: functions.Response) => {
@@ -60,7 +60,12 @@ export const importUserRegistrationData = functions.https.onRequest(
         emailAddress: elem[RegistrationColumns.EmailAddress],
         phoneNumber: elem[RegistrationColumns.PhoneNumber],
         isAvailableOnWhatsApp: elem[RegistrationColumns.WhatsApp],
-        languages: elem[RegistrationColumns.Languages].split(', '),
+        languages: elem[RegistrationColumns.Languages]
+          .split(', ')
+          .reduce((acc: any, language: string) => {
+            acc[language] = true;
+            return acc;
+          }, {}),
         services: elem[RegistrationColumns.Services],
         roles: {
           CR: elem[Roles.CR] === Decision.Yes,
@@ -96,56 +101,61 @@ export const importUserRegistrationData = functions.https.onRequest(
  * Process registration of webform data
  * 1. Registers user by transferring data into /users/ path
  * 2. Check if users already exists
- * 
+ *
  * @function processRegistration()
  */
 export const restructureRegistrationData = functions.database
-.ref('/webforms/registration/{registration_id}')
-.onCreate(async (snapshot, context) => {
-  const webform = snapshot.val();
+  .ref('/webforms/registration/{registration_id}')
+  .onCreate(async (snapshot, context) => {
+    const webform = snapshot.val();
 
-  const oldUser = await db.ref("/users/").orderByChild("/emailAddress")
-                  .equalTo(webform.email_address).once("value");
+    const oldUser = await db
+      .ref('/users/')
+      .orderByChild('/emailAddress')
+      .equalTo(webform.email_address)
+      .once('value');
 
-  // If the user already exists stop the execution and log the error
-  if (oldUser.val() !== undefined) {
-    console.warn(`Warning! User already exist with email: ${webform.email_address}`)
-    return false;
-  }
+    // If the user already exists stop the execution and log the error
+    if (oldUser.val() !== undefined) {
+      console.warn(
+        `Warning! User already exist with email: ${webform.email_address}`
+      );
+      return false;
+    }
 
-  const serviceRoles = {
-    "Content Reporting": "CR",
-    "Track Editing":     "TE",
-    "Sound Engineering": "SE",
-    "Quality Checking":  "QE",
-    "Fidelity Checking": "FC",
-  };
+    const serviceRoles = {
+      'Content Reporting': 'CR',
+      'Track Editing': 'TE',
+      'Sound Engineering': 'SE',
+      'Quality Checking': 'QE',
+      'Fidelity Checking': 'FC',
+    };
 
-  // if user email doesn't exists prepare the newUser Object
-  const newUser: any = {
-    timestamp: webform.completed,
-    emailAddress: webform.email_address,
-    phoneNumber: webform.contact_number,
-    isAvailableOnWhatsApp: webform.i_am_available_on_whatsapp === 1,
-    languages: webform.languages.reduce((result, language) => {
-      result[language] = true;
-      return result;
-    }, {}),
-    location: webform.country,
-    name: webform.your_name,
-    services: (<any>Object).values(webform.seva).join(", "),
-    experience: webform.experience,
-    influencer: webform.where_did_u_hear_about_this_seva,
-    recommendedBy: webform.recommended_by,
-    roles: webform.seva.reduce((result, service) => {
-      const code = serviceRoles[service];
-      result[code] = true;
+    // if user email doesn't exists prepare the newUser Object
+    const newUser: any = {
+      timestamp: webform.completed,
+      emailAddress: webform.email_address,
+      phoneNumber: webform.contact_number,
+      isAvailableOnWhatsApp: webform.i_am_available_on_whatsapp === 1,
+      languages: webform.languages.reduce((result, language) => {
+        result[language] = true;
+        return result;
+      }, {}),
+      location: webform.country,
+      name: webform.your_name,
+      services: (<any>Object).values(webform.seva).join(', '),
+      experience: webform.experience,
+      influencer: webform.where_did_u_hear_about_this_seva,
+      recommendedBy: webform.recommended_by,
+      roles: webform.seva.reduce((result, service) => {
+        const code = serviceRoles[service];
+        result[code] = true;
 
-      return result;
-    }, {})
-  }
+        return result;
+      }, {}),
+    };
 
-  db.ref(`/users/`).push(newUser);
+    db.ref(`/users/`).push(newUser);
 
-  return true;
-});
+    return true;
+  });
