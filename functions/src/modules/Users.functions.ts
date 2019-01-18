cbe6e0975e2318fbe6b99aa9cf330be71746759b
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-01-18 15:20:41 +0800

    Update functions/src/modules/Users.functions.ts
    Change elem -> row
    
    Co-Authored-By: adrienshen <aShenu0991@gmail.com>

--- functions/src/modules/Users.functions.ts
+++ functions/src/modules/Users.functions.ts
@@ -49,7 +49,7 @@ export const importUserRegistrationData = functions.https.onRequest(
 
     const registrationRows = await gsheets.getRows();
 
-    const readyForDatabaseUpdate = registrationRows.map((elem: any) => {
+    const readyForDatabaseUpdate = registrationRows.map((row: any) => {
       return {
         notes: withDefault(elem[RegistrationColumns.Details]),
         status: elem[RegistrationColumns.Status],
