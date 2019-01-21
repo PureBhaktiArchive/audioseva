3771b7ab8e9237424000c22e35b0b57723131aa9
adrienshen <adrienshen.dev@gmail.com>
2019-01-22 01:51:20 +0800

    Rewrite doneHandler

--- functions/src/modules/SQR.functions.ts
+++ functions/src/modules/SQR.functions.ts
@@ -538,3 +538,16 @@ export const exportSubmissionsToSpreadsheet = functions.database
       });
     }
   )
+
+export const processSQRDoneFromGmail = async (processObject: any) => {
+  console.log("processSubjectHeadersForSQR: ", processObject);
+  const listName = processObject.fileName.split("-")[0];
+  const soundQualityReporting = db.ref(`files/${listName}/${processObject.fileName}/soundQualityReporting`);
+  const sqrResults = await soundQualityReporting.once('value');
+  if (sqrResults.exists()) {
+    await soundQualityReporting.update({
+      status: "Done",
+      timestampDone: moment(processObject.lastThreadMessageTimestamp).unix(),
+    });
+  }
+}
