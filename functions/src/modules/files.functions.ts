d223faffcdf92b6f9e07b9eea5e3e57bdacd8869
Ahmad ElBatanouni <egyptstyles1@gmail.com>
2019-01-25 15:22:51 +0200

    Adapt the code to the new Storage structure

--- functions/src/modules/files.functions.ts
+++ functions/src/modules/files.functions.ts
@@ -4,22 +4,14 @@ import * as admin from 'firebase-admin';
 const bucket = admin.storage().bucket();
 const db = admin.database();
 
+import * as helpers from '../helpers';
 ///////////////////////////////////
 //   Sync-Related Helper Functions
 ///////////////////////////////////
 
-const checkValidMP3 = filePath =>
-  filePath.startsWith('mp3/') && filePath.endsWith('.mp3');
+const checkValidFile = filePath =>
+  filePath.match(/(mp3\/(?:.*\/)*(.*)\.(mp3)|source\/(?:.*\/)*(.*)\.(flac))/);
 
-const createMP3DBRef = filePath => {
-  const parts = filePath.split('/');
-
-  const list = parts[1];
-  const mp3 = parts[2];
-  const file_name = mp3.slice(0, -4);
-
-  return `/files/${list}/${file_name}`;
-};
 
 /////////////////////////////////////////////////
 //
@@ -28,73 +20,114 @@ const createMP3DBRef = filePath => {
 /////////////////////////////////////////////////
 
 export const importFilesFromStorage = functions.storage
+  .bucket(`original${functions.config().storage['root-domain']}`)
   .object()
   .onFinalize(object => {
     const filePath = object.name;
-    if (!checkValidMP3(filePath)) return 1;
+    const isValidFile = checkValidFile(filePath);//edited
+    if (!isValidFile)
+      throw new Error(`File "${filePath}" is not uploaded to the appropriate folder`);
 
-    const fileRef = db.ref(createMP3DBRef(filePath));
+    const fileName = isValidFile[2] || isValidFile[4];
+    const list = helpers.extractListFromFilename(fileName);
+
+    const fileRef = db.ref(`/files/${list}/${fileName}`);
 
     fileRef.set({
-      soundQualityReporting: { status: 'Spare' },
-      contentReporting: { status: 'Spare' },
+      mp3Uploaded: isValidFile[3] ? true : false,
+      flacUploaded: isValidFile[5] ? true : false,
     });
     return 1;
   });
 
-/////////////////////////////////////////////////
-//          Sync Storage to DB (HTTP Trigger)
-//
-//      1. Add the currently uploaded MP3s into the DB (handleCurrentlyUploadedFiles)
-//
-//      2. Remove DB entries for MP3s that don't exist (removeNonExistingMp3DBEntries)
-/////////////////////////////////////////////////
-export const syncStorageToDB = functions.https.onRequest(async (req, res) => {
-  ///////////////////////////////////////////////////////
-  //      1. Add the currently uploaded MP3s into the DB
-  ///////////////////////////////////////////////////////
-  const bucketFiles = await bucket.getFiles();
-  bucketFiles.forEach(innerFilesObject => {
-    innerFilesObject.forEach(file => {
-      if (!checkValidMP3(file.name)) return;
+export const HandleDeletedFiles = functions.storage
+  .bucket(`original${functions.config().storage['root-domain']}`)
+  .object()
+  .onDelete(async object => {
+    const filePath = object.name;
+    const nameParts = checkValidFile(filePath);
+    if (!nameParts) return -1;
 
-      const fileRef = db.ref(createMP3DBRef(file.name));
+    const fileName = nameParts[2] || nameParts[4];
+    const list = helpers.extractListFromFilename(fileName);
 
-      fileRef.set({
-        soundQualityReporting: { status: 'Spare' },
-        contentReporting: { status: 'Spare' },
-      });
-    });
+    let uploadType;
+    if (nameParts[3])
+      uploadType = 'mp3Uploaded';
+    else if (nameParts[5])
+      uploadType = 'flacUploaded';
+
+    const fileRef = db.ref(`/files/${list}/${fileName}`);
+
+    let update = {};
+    update[uploadType] = false;
+
+    return fileRef.update(update);
   });
 
-  ///////////////////////////////////////////////////////
-  //      2. Remove DB entries for MP3s that don't exist
-  ///////////////////////////////////////////////////////
-  const filesSnapshot = await db.ref(`/files`).once('value');
-  const files = filesSnapshot.val();
 
-  for (const list in files) {
-    for (const file in files[list]) {
-      const existingBucketFiles = await bucket
-        .file(`/mp3/${list}/${file}.mp3`)
-        .exists();
+/**
+ * syncStorageToDB 
+ * 1. Adds the currently uploaded audio files into the DB
+ * 2. Removes DB entries for files that no longer exist in the storage
+ */
+export const syncStorageToDB = functions.https
+  .onRequest(async (req, res) => {
 
-      // **Found** in DB but not in STORAGE
-      // Removing should be done only if the `status` is `Spare`
-      if (
-        !existingBucketFiles[0] &&
-        files[list][file]['soundQualityReporting'].status === 'Spare' &&
-        files[list][file]['contentReporting'].status === 'Spare'
-      ) {
-        db.ref(`/files/${list}/${file}`)
-          .remove()
-          .then(() => console.log('Deleted.'))
-          .catch(error => console.log(error));
+    //  1. Add the currently uploaded audio files into the DB
+    const bucketFiles = await admin
+      .storage()
+      .bucket(`original${functions.config().storage['root-domain']}`)
+      .getFiles();
+
+    const bucketFilePaths = bucketFiles[0]
+      .filter(file => checkValidFile(file.name))
+      .map(file => file.name);
+
+    bucketFilePaths.forEach(async filePath => {
+      const parts = checkValidFile(filePath);
+      const fileName = parts[2] || parts[4];
+      const list = helpers.extractListFromFilename(fileName);
+
+      const fileRef = db.ref(`/files/${list}/${fileName}`);
+
+      fileRef.update({
+        mp3Uploaded: parts[3] ? true : false,
+        flacUploaded: parts[5] ? true : false,
+      });
+    });
+
+
+    //  2.Remove DB entries for files that no longer exist
+    const filesSnapshot = await db.ref(`/files`).once("value");
+    const files = filesSnapshot.val();
+
+    const bucketFileNames = bucketFilePaths
+      .map(filePath => {
+        const fileName = checkValidFile(filePath)[2];
+        if (fileName)
+          return fileName;
+        else
+          return checkValidFile(filePath)[4]
+      });
+
+
+    for (const list in files) {
+      for (const fileName in files[list]) {
+        if (bucketFileNames.indexOf(fileName) < 0) // **Found** in DB but not in STORAGE
+          try {
+            if (
+              files[list][fileName]['soundQualityReporting'].status === 'Spare' &&
+              files[list][fileName]['contentReporting'].status === 'Spare'
+            )
+              db.ref(`/files/${list}/${fileName}`)
+                .remove();
+          } catch (err) {
+            console.warn(err)
+          }
       }
     }
-  }
-
-  return res.send(
-    `Started Execution, the process is now Running in the background`
-  );
-});
+    return res.send(
+      `Started Execution, Check the logs for any errors!`
+    );
+  });
