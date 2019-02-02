3074488da9ed5af2aec3f0481b9470c5cedfd0b4
Matthew Bernheimer <Krutonslol@gmail.com>
2019-02-02 00:34:15 -0500

    Add SQR Submission type

--- types/SQR.ts
+++ types/SQR.ts
@@ -1,5 +1,20 @@
 import { IBaseAllotment } from "./Common";
+import { ISoundIssue, IUnwantedPart } from "./SE";
 
 export interface ISQRRawAllotment extends IBaseAllotment {
   files: string[]
+}
+
+export interface ISQRSubmission {
+  unwantedParts: IUnwantedPart[];
+  soundIssues: ISoundIssue[];
+  duration: {
+    beginning: any;
+    ending: any;
+  };
+  soundQualityRating: "Good" | "Average" | "Bad" | "Inaudible" | "Blank";
+  comments: string;
+  created: number;
+  changed: number;
+  completed?: number;
 }
