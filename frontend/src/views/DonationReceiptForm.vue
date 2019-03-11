017e850d2339d65ae6a35a2dd4aa21bfda81ebc3
Matthew Bernheimer <Krutonslol@gmail.com>
2019-03-11 13:08:28 -0400

    Rename DonationForm to DonationReceiptForm

--- frontend/src/views/DonationReceiptForm.vue
+++ frontend/src/views/DonationReceiptForm.vue
@@ -98,7 +98,7 @@ import VuePhoneNumberInput from "vue-phone-number-input";
 import "vue-phone-number-input/dist/vue-phone-number-input.css";
 
 @Component({
-  name: "DonationForm",
+  name: "DonationReceiptForm",
   components: { VuePhoneNumberInput }
 })
 export default class DonationForm extends Vue {
