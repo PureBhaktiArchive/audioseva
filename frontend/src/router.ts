09d3e8184ebe5e63cffd8d5fd31cadcc2fed28da
Matthew Bernheimer <Krutonslol@gmail.com>
2019-03-29 07:46:22 -0400

    Change track editing menu icon

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -158,7 +158,7 @@ export const router = new Router({
           meta: {
             activator: true,
             activatorName: "Track Editing",
-            menuIcon: "fas fa-headphones"
+            menuIcon: "fas fa-cut"
           },
           children: [
             {
