e125bb6a893fb645188c5e4664ca808c27882bbd
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-05-22 21:03:27 +0700

    Refactor TE workflow processing.

--- functions/src/modules/TE.functions.ts
+++ functions/src/modules/TE.functions.ts
@@ -1,8 +1,12 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as moment from 'moment';
-import * as helpers from './../helpers';
 import * as path from 'path';
+import * as helpers from './../helpers';
+
+const rootBucketDomain = functions.config().storage['root-domain'];
+const teUploadsBucket = `te.uploads.${rootBucketDomain}`;
+const editedBucket = `edited.${rootBucketDomain}`;
 
 /**
  * Saves allotment to the db and sends an email notification
@@ -58,14 +62,13 @@ export const processAllotment = functions.https.onCall(
         // Getting the tasks list to be used when notifying the assignee
         const task = (await taskRef.once('value')).val();
 
-        //inject filename and sourceFileLink into the task object returned by db call
-        task.fileName = taskId;
+        //inject taskId and sourceFileLink into the task object returned by db call
+        task.taskId = taskId;
         task.chunks.forEach(chunk => {
-          chunk.sourceFileLink = `https://edited.${
-            functions.config().storage['root-domain']
-          }/${helpers.extractListFromFilename(chunk.fileName)}/${
-            chunk.fileName
-          }`;
+          chunk.sourceFileLink = `http://storage.googleapis.com/
+            ${editedBucket}/
+            ${helpers.extractListFromFilename(chunk.taskId)}/
+            ${chunk.taskId}`;
         });
         return task;
       })
@@ -94,62 +97,43 @@ export const processAllotment = functions.https.onCall(
   }
 );
 
-const teUploadsBucket = `https://te.uploads.${
-  functions.config().storage['root-domain']
-}`;
-
-const editedBucket = `https://edited.${
-  functions.config().storage['root-domain']
-}`;
-
-const addTeSubmissionWarnings = (
-  isAssignedToUser: boolean,
-  isStatusDone: boolean
-): string[] => {
-  const warnings = [];
-  if (!isAssignedToUser) warnings.push('User is not assigned to the task!');
-  if (isStatusDone) warnings.push('Task is already in the Done status!');
-
-  return warnings;
-};
-
 export const processSubmission = functions.storage
   .bucket(teUploadsBucket)
   .object()
   .onFinalize(async (object, context) => {
-    const filePath = object.name;
+    const user = await admin.auth().getUser(context.auth.uid);
 
-    const teUser = admin.auth().getUser(context.auth.uid);
-
-    const isDisabled = await teUser.then(x => x.disabled);
-    if (isDisabled) {
+    if (user.disabled) {
       throw new Error(`Invalid User`);
     }
 
-    const emailAddress = await teUser.then(x => x.email);
-    const fileName = path.basename(filePath);
-    const list = helpers.extractListFromFilename(fileName);
+    const taskId = path.basename(object.name, '.flac');
+    const list = helpers.extractListFromFilename(taskId);
 
     const taskRef = admin
       .database()
-      .ref(`/edited/${list}/${fileName}/trackEditing`);
+      .ref(`/edited/${list}/${taskId}/trackEditing`);
 
-    // Getting the tasks list to be used when notifying the user
     const task = (await taskRef.once('value')).val();
 
-    const warnings = addTeSubmissionWarnings(
-      task.assignee.emailAddress === emailAddress,
-      task.status
-    );
+    const warnings = [];
+    if (task.assignee.emailAddress !== user.email)
+      warnings.push(`Task is assigned to ${task.assignee.emailAddress}, uploaded by ${user.email}.`);
+    if (task.status === 'Done')
+      warnings.push('Task is already Done.');
 
     //update task if task status is not done
     if (task.status.toLowerCase() !== 'done') {
       await taskRef.update({
-        uploadPath: filePath,
+        uploadPath: object.name,
         status: 'Submitted',
         submissionTimestamp: moment().format(),
       });
     }
+
+    // Injecting additional fields for email notification
+    task.id = taskId;
+    task.uploadURL = `https://storage.googleapis.com/${teUploadsBucket}/${object.name}`;
 
     // Notify the user
     await admin
@@ -157,10 +141,9 @@ export const processSubmission = functions.storage
       .ref(`/email/notifications`)
       .push({
         template: 'te-submission-email',
-        to: emailAddress,
+        to: user.email,
         params: {
           task,
-          taskId: fileName,
           warnings,
         },
       });
@@ -173,20 +156,17 @@ export const processFeedback = functions.database
       change: functions.Change<functions.database.DataSnapshot>,
       context: functions.EventContext
     ): Promise<any> => {
-      const { list } = context.params;
-      const { taskId } = context.params;
-
-      const taskRef = admin
-        .database()
-        .ref(`/edited/${list}/${taskId}/trackEditing`);
+      const taskRef = change.before.ref.parent;
 
       await taskRef.update({
         status: 'Revise',
         feedbackTimestamp: moment().format(),
       });
 
-      // Getting the tasks list to be used when notifying the user
       const task = (await taskRef.once('value')).val();
+
+      // Injecting additional fields for email notification
+      task.id = context.params.taskId;
 
       return admin
         .database()
@@ -195,8 +175,7 @@ export const processFeedback = functions.database
           to: task.assignee.emailAddress,
           template: 'te-feedback-email',
           params: {
-            task,
-            taskId,
+            task
           },
         });
     }
@@ -215,24 +194,18 @@ export const processApproval = functions.database
       if (!(statusBefore === 'Submitted' && statusAfter === 'Done'))
         return null;
 
-      const { list } = context.params;
-      const { taskId } = context.params;
-
-      const taskRef = admin
-        .database()
-        .ref(`/edited/${list}/${taskId}/trackEditing`);
+      const taskRef = change.before.ref.parent;
 
       await taskRef.update({
         status: 'Done',
         doneTimestamp: moment().format(),
       });
 
-      const storage = admin.storage();
+      const task = (await taskRef.once('value')).val();
 
-      //copy file from upload bucked to edited bucket
-      await storage
+      await admin.storage()
         .bucket(teUploadsBucket)
-        .file(taskId)
-        .copy(storage.bucket(editedBucket).file(taskId));
+        .file(task.uploadPath)
+        .copy(admin.storage().bucket(editedBucket));
     }
   );
