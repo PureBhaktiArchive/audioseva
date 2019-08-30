c12905d4dec58494c287393909e4bd8760809bee
Matthew Bernheimer <Krutonslol@gmail.com>
2019-08-30 04:29:27 -0400

    Validate file in Listen view

--- frontend/src/views/Listen.vue
+++ frontend/src/views/Listen.vue
@@ -4,46 +4,51 @@ import { functions } from 'firebase';
  */
 <template>
   <div>
-    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" fixed app>
-      <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer">
-          <h3>Audio File Name: {{ audioFileName }}</h3>
-        </router-link>
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card color="#d9edf7">
-      <v-card-title primary-title>
-        <div>
-          <h3 class="headline mb-0">Audio Player</h3>
-        </div>
-      </v-card-title>
-      <v-card-title primary-title>
-        <audio controls="controls" :src="audioUrl" style="display: block; width: 100%;">
-          Your browser does not support embedding audio. Please click
-          <a :href="audioUrl">this link</a>.
-        </audio>
-      </v-card-title>
-      <v-card-title primary-title>
-        <div>
-          <small>
-            * To download the file, please click on the three dots on the right of the player above and choose ‘Download’. Please note that your browser may display the download option with the download icon instead or disallow downloading. Alternatively, you can long-tap the following link:
-            <a
-              :href="audioUrl"
-            >Download</a>.
-          </small>
-        </div>
-      </v-card-title>
-      <v-card>
+    <div v-if="errorMessage">
+      {{ errorMessage }}
+    </div>
+    <div v-else>
+      <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" fixed app>
+        <v-toolbar-title>
+          <router-link to="/" tag="span" style="cursor: pointer">
+            <h3>Audio File Name: {{ audioFileName }}</h3>
+          </router-link>
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card color="#d9edf7">
         <v-card-title primary-title>
           <div>
-            <p class="text-justify">
-              When you fill the Online Submission Form, please provide as many details as you can especially about sound quality because we are depending entirely on your feedback to process these files in the sound editing stage. Please
-              mention if there is any background noise, abrupt sounds, blank spaces, low sound volume, etc. If you miss to provide some input, chances are, the issue will be part of the final archive.
-            </p>
+            <h3 class="headline mb-0">Audio Player</h3>
           </div>
         </v-card-title>
+        <v-card-title primary-title>
+          <audio controls="controls" :src="audioUrl" style="display: block; width: 100%;">
+            Your browser does not support embedding audio. Please click
+            <a :href="audioUrl">this link</a>.
+          </audio>
+        </v-card-title>
+        <v-card-title primary-title>
+          <div>
+            <small>
+              * To download the file, please click on the three dots on the right of the player above and choose ‘Download’. Please note that your browser may display the download option with the download icon instead or disallow downloading. Alternatively, you can long-tap the following link:
+              <a
+                :href="audioUrl"
+              >Download</a>.
+            </small>
+          </div>
+        </v-card-title>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <p class="text-justify">
+                When you fill the Online Submission Form, please provide as many details as you can especially about sound quality because we are depending entirely on your feedback to process these files in the sound editing stage. Please
+                mention if there is any background noise, abrupt sounds, blank spaces, low sound volume, etc. If you miss to provide some input, chances are, the issue will be part of the final archive.
+              </p>
+            </div>
+          </v-card-title>
+        </v-card>
       </v-card>
-    </v-card>
+    </div>
   </div>
 </template>
 
@@ -58,13 +63,38 @@ import { Component, Vue } from "vue-property-decorator";
 })
 export default class ListenAudio extends Vue {
   fileName: string = "";
+  errorMessage = "";
+  audio!: HTMLMediaElement;
 
   mounted() {
     this.fileName = this.$route.params.fileName;
+    this.audio = document.querySelector("audio") as HTMLMediaElement;
+    if (this.isValidFileExtension()) {
+      this.audio.addEventListener("error", this.handleFileError);
+    }
+  }
+
+  destroyed() {
+    this.audio.removeEventListener("error", this.handleFileError);
+  }
+
+  handleFileError(e: any) {
+    if (e.target.error.code === 4) {
+      this.errorMessage = "File does not exist.";
+    }
   }
 
   get nameAndExtension() {
     return this.fileName.split(".");
+  }
+
+  isValidFileExtension() {
+    if (["mp3", "flac"].includes(this.fileName.split(".")[1])) {
+      return true;
+    } else {
+      this.errorMessage = "File extension must be mp3 or flac.";
+      return false;
+    }
   }
 
   get audioUrl() {
