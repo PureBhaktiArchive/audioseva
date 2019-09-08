9e90a5b0d365ab0f80a35a873fcb65f77ea1ecbe
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-09-08 11:55:06 +0700

    Adjust TE resolution processing.

--- functions/src/StorageManager.ts
+++ functions/src/StorageManager.ts
@@ -8,7 +8,7 @@ export class StorageManager {
   public static rootBucketDomain = functions.config().project.domain;
   public static originalFilesBucket = `original.${StorageManager.rootBucketDomain}`;
   public static trackEditedUploadsBucket = `te.uploads.${StorageManager.rootBucketDomain}`;
-  public static trackEditedFilesBucket = `edited.${StorageManager.rootBucketDomain}`;
+  public static trackEditedFinalBucket = `edited.${StorageManager.rootBucketDomain}`;
 
   static getPublicURL(bucket: string, filePath: string): any {
     return `https://storage.googleapis.com/${bucket}/${filePath}`;
