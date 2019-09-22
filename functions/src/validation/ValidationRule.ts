680f79900d0fa58b0530fad7a899f07ba0eaf6ad
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-09-22 21:08:29 +0700

    Add validation framework.

---
+++ functions/src/validation/ValidationRule.ts
@@ -0,0 +1,24 @@
+/*!
+ * sri sri guru gauranga jayatah
+ */
+
+import { IValidationRule } from './IValidationRule';
+
+export class ValidationRule<T> implements IValidationRule<T> {
+  constructor(private spec: (entity: T) => boolean, public message: string) {}
+
+  public validate(entity: T): boolean {
+    return this.spec(entity);
+  }
+}
+
+export class ValidationRuleForEach<T> implements IValidationRule<T[]> {
+  constructor(
+    private spec: (value: T, index: number, array: T[]) => boolean,
+    public message: string
+  ) {}
+
+  validate(entities: T[]): boolean {
+    return entities.every(this.spec);
+  }
+}
