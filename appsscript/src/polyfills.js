d32afbc1c18161c1403ee7d3c1d698dc82836dd3
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-09-28 15:53:44 +0700

    Remove appscript project.

--- appsscript/src/polyfills.js
+++
@@ -1,6 +0,0 @@
-/* eslint no-extend-native: "off" */
-if (!String.prototype.startsWith) {
-  String.prototype.startsWith = function startsWith(search, pos) {
-    return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
-  };
-}
