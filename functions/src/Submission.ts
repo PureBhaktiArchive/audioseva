fc70d6206fb85ab0ee100fc506379b33993f4ab8
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-10-20 23:16:05 +0700

    Save author to the final SQR submission.

--- functions/src/Submission.ts
+++ functions/src/Submission.ts
@@ -2,7 +2,10 @@
  * sri sri guru gauranga jayatah
  */
 
+import { Assignee } from './Assignee';
+
 export interface Submission {
+  author?: Assignee;
   created: number;
   changed: number;
   completed?: number;
