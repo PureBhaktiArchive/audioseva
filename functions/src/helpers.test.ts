b13cef895734ef8d8e1577cfd0a800356fcce59f
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-10-26 18:59:17 +0700

    Add file URLs handling.

--- functions/src/helpers.test.ts
+++ functions/src/helpers.test.ts
@@ -1,4 +1,4 @@
-import { extractListFromFilename } from './helpers';
+import { extractListFromFilename, standardizeFileName } from './helpers';
 
 describe('List extraction', () => {
   test.each`
@@ -9,5 +9,18 @@ describe('List extraction', () => {
     ${'19960528LEICESTER_T36'} | ${null}
   `('extracts $list from $fileName', ({ fileName, list }) => {
     expect(extractListFromFilename(fileName)).toEqual(list);
+  });
+});
+
+describe('File name standardization', () => {
+  test.each`
+    input         | standard
+    ${'Hi3 A'}    | ${'ML1-003A'}
+    ${'BR-01A'}   | ${'BR-001A'}
+    ${'DK-1A'}    | ${'DK-001A'}
+    ${'SER-88A'}  | ${'SER-088A'}
+    ${'ML2-71 A'} | ${'ML2-0071A'}
+  `('$input → $standard', ({ input, standard }) => {
+    expect(standardizeFileName(input)).toEqual(standard);
   });
 });
