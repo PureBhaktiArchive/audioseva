eec1bc89373ed954ee5401bd9bd1970a93f80167
Matthew Bernheimer <Krutonslol@gmail.com>
2019-11-08 21:46:32 -0500

    Add stories for SQRForm components

--- frontend/.storybook/stories/SQRForm.stories.js
+++ frontend/.storybook/stories/SQRForm.stories.js
@@ -2,18 +2,20 @@ import { object } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import { storyFactory } from "../util/helpers";
 import SoundIssues from "../../src/components/SQRForm/SoundIssues";
+import UnwantedParts from "../../src/components/SQRForm/UnwantedParts";
 
-export default { title: "SoundIssues" }
+export default { title: "SQRForm" }
 
 const story = storyFactory({
-  SoundIssues
+  SoundIssues,
+  UnwantedParts
 });
 
-export const asDefault = () => story({
+const formData = (path) => ({
   props: {
     form: {
       default: {
-        soundIssues: object("soundIssues", [
+        [path]: object(path, [
           { beginning: "12:30", type: "other type" },
           { beginning: "13:55" }
         ])
@@ -23,7 +25,11 @@ export const asDefault = () => story({
   methods: {
     updateForm: action("update form"),
     removeField: action("remove field")
-  },
+  }
+});
+
+export const soundIssues = () => story({
+  ...formData("soundIssues"),
   template: `
     <sound-issues
       :form="form"
@@ -31,4 +37,15 @@ export const asDefault = () => story({
       :removeField="removeField"
     ></sound-issues>
 `
+});
+
+export const unwantedParts = () => story({
+  ...formData("unwantedParts"),
+  template: `
+    <unwanted-parts 
+      :form="form" 
+      :updateForm="updateForm" 
+      :removeField="removeField"
+    ></unwanted-parts>
+  `
 });
