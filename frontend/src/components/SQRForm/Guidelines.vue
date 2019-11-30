692f52cb7a1f716cae79b22a3010b1ad46fea983
DadImScared <Krutonslol@gmail.com>
2019-11-30 01:08:58 -0500

    Refactor SQR fields (#271)
    * Move form fields into template
    * Move cancel fields into component
    * Move guidelines into separate component
    * Lint SQR/Form

---
+++ frontend/src/components/SQRForm/Guidelines.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-expansion-panels class="pt-3">
+    <v-expansion-panel>
+      <v-expansion-panel-header class="pl-0"
+        >Guidelines</v-expansion-panel-header
+      >
+      <v-expansion-panel-content>
+        <slot></slot>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Guidelines"
+})
+export default class GuideLines extends Vue {}
+</script>
+
+<style scoped></style>
