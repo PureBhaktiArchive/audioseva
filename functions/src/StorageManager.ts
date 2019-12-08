406b3611edd1c94c3a928c80f3d28133df3959f7
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-12-08 13:50:41 +0700

    Move helper methods down.

--- functions/src/StorageManager.ts
+++ functions/src/StorageManager.ts
@@ -11,40 +11,6 @@ import admin = require('firebase-admin');
 export type BucketName = 'original' | 'edited' | 'restored' | 'te.uploads';
 
 export class StorageManager {
-  static extractListFromFilename = (fileName: string): string => {
-    const match = fileName.match(/^\w+(?=-)|Hi(?=\d)/i);
-    if (!match) return null;
-
-    const list = match[0].toUpperCase();
-    return list === 'HI' ? 'ML1' : list;
-  };
-
-  static standardizeFileName = (fileName: string) => {
-    if (fileName.startsWith('ML2-')) return fileName.replace(/^ML2-/, '');
-
-    return fileName
-      .replace(/^Hi/i, 'ML1-')
-      .replace(
-        /^(ML[12]|[a-zA-Z]+)-(\d{1,4})\s*([\w\s]*)(\.\w{3,4})$/i,
-        (
-          match,
-          list: string,
-          serial: string,
-          suffix: string,
-          extension: string,
-          index,
-          original
-        ) =>
-          [
-            list.toUpperCase(),
-            '-',
-            serial.padStart(list === 'ML2' ? 4 : 3, '0'),
-            suffix.toUpperCase().replace(/[-\s]/g, ''),
-            extension,
-          ].join('')
-      );
-  };
-
   static getFullBucketName(bucketName: BucketName) {
     return `${bucketName}.${functions.config().project.domain}`;
   }
@@ -98,4 +64,38 @@ export class StorageManager {
     for (const file of files) if ((await file.exists())[0]) return file;
     return null;
   }
+
+  static extractListFromFilename = (fileName: string): string => {
+    const match = fileName.match(/^\w+(?=-)|Hi(?=\d)/i);
+    if (!match) return null;
+
+    const list = match[0].toUpperCase();
+    return list === 'HI' ? 'ML1' : list;
+  };
+
+  static standardizeFileName = (fileName: string) => {
+    if (fileName.startsWith('ML2-')) return fileName.replace(/^ML2-/, '');
+
+    return fileName
+      .replace(/^Hi/i, 'ML1-')
+      .replace(
+        /^(ML[12]|[a-zA-Z]+)-(\d{1,4})\s*([\w\s]*)(\.\w{3,4})$/i,
+        (
+          match,
+          list: string,
+          serial: string,
+          suffix: string,
+          extension: string,
+          index,
+          original
+        ) =>
+          [
+            list.toUpperCase(),
+            '-',
+            serial.padStart(list === 'ML2' ? 4 : 3, '0'),
+            suffix.toUpperCase().replace(/[-\s]/g, ''),
+            extension,
+          ].join('')
+      );
+  };
 }
