1cecd1e03e9050509fce6478faf697693b4df45f
Matthew Bernheimer <Krutonslol@gmail.com>
2019-12-19 18:45:53 -0500

    Show random quote on loading screen instead of loading text

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,28 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/database";
 export default {
   data() {
-    return {};
+    return {
+      quotes: []
+    };
+  },
+  mounted() {
+    this.getQuotes();
+  },
+  methods: {
+    async getQuotes() {
+      await this.$rtdbBind("quotes", firebase.database().ref("quotes"));
+      localStorage.setItem(
+        "quotes",
+        JSON.stringify([
+          "I am closer to you than the air you breathe.",
+          ...this.quotes.map(quote => quote.text)
+        ])
+      );
+    }
   }
 };
 </script>
