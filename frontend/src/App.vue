86806d40cc947c1d1eec9ffbf0cf868cc9d7d3bf
DadImScared <Krutonslol@gmail.com>
2019-12-19 21:32:10 -0500

    Show Srila Gurudeva's quotes on the loading screen (#304)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,25 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/database";
 export default {
   data() {
-    return {};
+    return {
+      quotes: []
+    };
+  },
+  mounted() {
+    this.getQuotes();
+  },
+  methods: {
+    async getQuotes() {
+      await this.$rtdbBind("quotes", firebase.database().ref("quotes"));
+      localStorage.setItem(
+        "quotes",
+        JSON.stringify(this.quotes.map(quote => quote.text))
+      );
+    }
   }
 };
 </script>
