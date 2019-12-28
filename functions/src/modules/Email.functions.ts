c432f4df80bcec767575993d55109da2b22ed886
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2019-12-28 10:52:30 +0400

    Send Emails using SMTP channel.

--- functions/src/modules/Email.functions.ts
+++ functions/src/modules/Email.functions.ts
@@ -1,22 +1,24 @@
 import * as functions from 'firebase-functions';
 import { DateTime } from 'luxon';
 import Email = require('email-templates');
-import sendinBlue = require('nodemailer-sendinblue-transport');
 import admin = require('firebase-admin');
 
 const email = new Email({
-  transport: sendinBlue({ apiKey: functions.config().send_in_blue || '' }),
+  message: {
+    from: functions.config().email.sender,
+  },
+  transport: functions.config().email.connection,
 });
 
 export const sendNotificationEmail = functions.database
   .ref('/email/notifications/{pushId}')
-  .onCreate(async snapshot => {
+  .onCreate(async (snapshot, { params }) => {
     const data = snapshot.val();
 
     if (data.sentTimestamp) return;
 
     console.log(
-      `Sending an email to ${data.to} with template "${data.template}"`
+      `Sending email ${params.pushId} to ${data.to} with template "${data.template}"`
     );
 
     await email.send({
