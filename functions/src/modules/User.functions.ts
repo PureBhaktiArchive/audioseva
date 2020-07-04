85eb12bb1190d38f5a1a27fae1ab88c7b7b4b391
DadImScared <Krutonslol@gmail.com>
2020-07-04 08:49:39 -0400

    Apply user roles changes instantly (#351)
    Co-authored-by: Ananta Krsna dasa <anantakrishna.kem@gmail.com>

--- functions/src/modules/User.functions.ts
+++ functions/src/modules/User.functions.ts
@@ -181,6 +181,11 @@ export const updateUserClaims = functions.database
     await admin
       .auth()
       .setCustomUserClaims(user.uid, { roles: change.after.val() });
+
+    /// Triggering refresh on the client.
+    await change.after.ref.parent
+      .child('refreshTime')
+      .set(admin.database.ServerValue.TIMESTAMP);
   });
 
 export const addNewUserToDatabase = functions.auth
