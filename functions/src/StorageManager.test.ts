7b0c9cdb919bea4988cc94f8e784abae56379aae
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2020-08-10 16:59:43 +0700

    Refine the regex to accomodate edge cases.

--- functions/src/StorageManager.test.ts
+++ functions/src/StorageManager.test.ts
@@ -47,21 +47,32 @@ describe('File', () => {
 
 describe('Uploaded SE file', () => {
   test.each`
-    sourceFileName               | destinationFileName
-    ${'folder/ISK-22-2_v2.flac'} | ${'ISK/ISK-22-2.flac'}
-    ${'a/b/c/ML2-1059-1.flac'}   | ${'ML2/ML2-1059-1.flac'}
-    ${'ML2-1113-1_v2-2.flac'}    | ${'ML2/ML2-1113-1.flac'}
-    ${'ML2-97 B_V2.flac'}        | ${'ML2/ML2-97 B.flac'}
-    ${'BR-03B v3.flac'}          | ${'BR/BR-03B.flac'}
-    ${'DIGI07-0001-1.mp3'}       | ${'DIGI07/DIGI07-0001-1.mp3'}
+    sourceFileName                       | destinationFileName
+    ${'folder/ISK-22-2_v2.flac'}         | ${'ISK/ISK-22-2.flac'}
+    ${'a/b/c/ML2-1059-1.flac'}           | ${'ML2/ML2-1059-1.flac'}
+    ${'ML2-1113-1_v2-2.flac'}            | ${'ML2/ML2-1113-1.flac'}
+    ${'ML2-97 B_V2.flac'}                | ${'ML2/ML2-97 B.flac'}
+    ${'BR-03B v3.flac'}                  | ${'BR/BR-03B.flac'}
+    ${'DIGI07-0001-1.mp3'}               | ${'DIGI07/DIGI07-0001-1.mp3'}
+    ${'DIGI07-0306-1_v2_less_harsh.mp3'} | ${'DIGI07/DIGI07-0306-1.mp3'}
+    ${'DIGI08-0015_V3 Sadananda.mp3'}    | ${'DIGI08/DIGI08-0015.mp3'}
+    ${'DIGI07-0214-1_Sadananda.mp3'}     | ${'DIGI07/DIGI07-0214-1.mp3'}
+    ${'DIGI04-0047-1_v1.2.mp3'}          | ${'DIGI04/DIGI04-0047-1.mp3'}
+    ${'DIGI04-0047-1 Sadanand.mp3'}      | ${'DIGI04/DIGI04-0047-1.mp3'}
+    ${'BR-49A-2.mp3'}                    | ${null}
+    ${'RANI-93-2.flac.flac'}             | ${null}
   `(
     `$sourceFileName should be saved to $destinationFileName`,
     ({ sourceFileName, destinationFileName }) => {
       const file = StorageManager.getDestinationFileForRestoredUpload(
         sourceFileName
       );
-      expect(file).not.toBeNull();
-      expect(file.name).toEqual(destinationFileName);
+      if (destinationFileName) {
+        expect(file).not.toBeNull();
+        expect(file.name).toEqual(destinationFileName);
+      } else {
+        expect(file).toBeNull();
+      }
     }
   );
 });
