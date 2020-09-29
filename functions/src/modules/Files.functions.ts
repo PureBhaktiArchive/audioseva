65454a8e3bda2c56c0a10465c73374884c00013a
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2020-09-29 14:44:56 +0700

    Implement using Pub/Sub.

--- functions/src/modules/Files.functions.ts
+++ functions/src/modules/Files.functions.ts
@@ -2,6 +2,7 @@
  * sri sri guru gauranga jayatah
  */
 
+import { PubSub } from '@google-cloud/pubsub';
 import { File } from '@google-cloud/storage';
 import { database } from 'firebase-admin';
 import getAudioDurationInSeconds from 'get-audio-duration';
@@ -10,12 +11,12 @@ import { Readable } from 'stream';
 import { promisify } from 'util';
 import { Spreadsheet } from '../Spreadsheet';
 import { BucketName, StorageManager } from '../StorageManager';
-import pProps = require('p-props');
 import functions = require('firebase-functions');
 import express = require('express');
 import path = require('path');
-import _ = require('lodash');
 import mm = require('musicmetadata');
+import admin = require('firebase-admin');
+import pMap = require('p-map');
 
 const app = express();
 
@@ -67,109 +68,121 @@ export const download = functions
   .runWith({ memory: '128MB' })
   .https.onRequest(app);
 
-interface FileMetadata {
-  duration: number;
-  size: number;
-  updated: number;
-  crc32c: string;
-  md5Hash: string;
-}
-type FileName = string;
-type GenerationNumber = number;
-type Dump = Record<
-  BucketName,
-  Record<FileName, Record<GenerationNumber, FileMetadata>>
->;
+// interface FileMetadata {
+//   duration: number;
+//   size: number;
+//   updated: number;
+//   crc32c: string;
+//   md5Hash: string;
+// }
+// type FileName = string;
+// type GenerationNumber = number;
+// type Dump = Record<
+//   BucketName,
+//   Record<FileName, Record<GenerationNumber, FileMetadata>>
+// >;
 
-const getAudioMetadata = promisify<Readable, MM.Options, MM.Metadata>(mm);
-const getAudioDuration = getAudioDurationInSeconds;
+const rootFilesMetadataRef = database().ref('files');
+const getFileDurationLeaf = <
+  T extends database.Reference | database.DataSnapshot
+>(
+  root: T,
+  file: File
+): T =>
+  root
+    .child(file.bucket.name.split('.')[0])
+    .child(path.basename(file.name, path.extname(file.name)))
+    .child(file.generation.toString())
+    .child('duration') as T;
 
-export const dumpMetadata = functions
-  .runWith({ timeoutSeconds: 540 })
+const pubsub = new PubSub();
+const TOPIC_NAME = 'dump-duration';
+
+export const enqueueFilesDurationsExtraction = functions
+  .runWith({ timeoutSeconds: 120 })
   .pubsub.schedule('every day 23:00')
   .timeZone(functions.config().coordinator.timezone)
   .onRun(async () => {
-    const dbref = database().ref('files');
-    const existingDump = (await dbref.once('value')).val() as Dump;
+    const snapshot = await rootFilesMetadataRef.once('value');
+    const topic = pubsub.topic(TOPIC_NAME);
 
-    const files = _(
-      await Promise.all(
-        ['edited', 'restored'].map(async (bucketName: BucketName) => {
-          const [files] = await StorageManager.getBucket(bucketName).getFiles({
-            versions: true,
-          });
-          return files;
-        })
-      )
-    )
-      .flatten()
-      .take(200)
-      .reduce<Record<string, File>>((accumulator, file) => {
-        const shortBucketName = file.bucket.name.split('.')[0];
-        const baseFileName = path.basename(file.name, path.extname(file.name));
-
+    await pMap(['edited', 'restored'], async (bucketName: BucketName) => {
+      const [files] = await StorageManager.getBucket(bucketName).getFiles({
+        versions: true,
+      });
+      await pMap(files.slice(0, 200), async (file) => {
         if (
           // Skipping folder objects
           !file.name.endsWith('/') &&
-          // Skipping files for which the metadata is already dumped
-          !existingDump?.[shortBucketName]?.[baseFileName]?.[file.generation]
-            ?.duration
+          // Skipping files for which the duration is already dumped
+          !getFileDurationLeaf(snapshot, file).exists()
         )
-          accumulator[
-            `${shortBucketName}/${baseFileName}/${file.generation}`
-          ] = file;
-        return accumulator;
-      }, {});
+          await topic.publishJSON({
+            bucketName: file.bucket.name,
+            fileName: file.name,
+            generation: file.generation,
+          });
+      });
+    });
+  });
 
-    const updates = await pProps(
-      files,
-      async (file) => {
-        const { size, updated, crc32c, md5Hash } = file.metadata;
+const mmAsync = promisify<Readable, MM.Options, MM.Metadata>(mm);
+const getAudioDurationUsingMM = (stream: Readable, fileSize: number) =>
+  mmAsync(stream, {
+    duration: true,
+    fileSize,
+  }).then(({ duration }) => duration);
 
-        const stream = file.createReadStream();
-        stream.on('error', (error) =>
-          console.error(
-            'Error in stream',
-            file.bucket.name,
-            file.name,
-            file.generation,
-            error.message
-          )
-          );
+export const dumpDuration = functions.pubsub
+  .topic(TOPIC_NAME)
+  .onPublish(async (message, context) => {
+    const { bucketName, fileName, generation } = message.json;
 
-        // const duration = await getAudioDuration(stream).catch((error) => {
-        //   console.error(
-        //     `Error getting audio duration for ${file.bucket.name}/${file.name}#${file.generation} (${file.metadata.md5Hash})`,
-        //     error.message
-        //   );
-        //   // If no duration is obtained, return null
-        //   return null;
-        // });
-        const { duration } = await getAudioMetadata(stream, {
-          duration: true,
-          fileSize: size,
-        }).catch((error) => {
-          console.error(
-            `Error getting audio metadata for ${file.bucket.name}/${file.name}#${file.generation} (${file.metadata.md5Hash})`,
-            error.message
-          );
-          // If no duration is obtained, return null
-          return { duration: null };
-        });
+    const file = admin
+      .storage()
+      .bucket(bucketName)
+      .file(fileName, { generation });
 
-        return {
-          name: file.name,
-          size,
-          updated: new Date(updated).valueOf(),
-          crc32c,
-          md5Hash,
-          duration,
-        };
-      },
-      { concurrency: 5, stopOnError: false }
+    await file.getMetadata();
+
+    const { size, updated, crc32c, md5Hash } = file.metadata;
+
+    const stream = file.createReadStream();
+    stream.on('error', (error) =>
+      console.error(
+        'Error in stream',
+        file.bucket.name,
+        file.name,
+        file.generation,
+        error.message
+      )
     );
 
-    await dbref.update(updates);
+    try {
+      console.log(
+        `Trying to get audio duration for ${file.bucket.name}/${file.name}#${file.generation} (${file.metadata.md5Hash})`
+      );
+      const duration =
+        (await getAudioDurationInSeconds(stream).catch(() => null)) ||
+        (await getAudioDurationUsingMM(stream, size).catch(() => null));
+
+      await getFileDurationLeaf(rootFilesMetadataRef, file).parent.set({
+        name: file.name,
+        size,
+        updated: new Date(updated).valueOf(),
+        crc32c,
+        md5Hash,
+        duration,
+      });
+    } catch (error) {
+      console.error('Error getting audio duration', error.message);
+    }
+  });
+
+export const dumpDurationsToSpreadsheet = functions.pubsub
+  .schedule('every day 04:00')
+  .timeZone(functions.config().coordinator.timezone)
+  .onRun(async () => {
     interface DurationsRow {
       'File Name': string;
       Duration: number;
