16d5d33850a7045b8172f80f7315f9a1439357c3
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2022-08-31 10:49:00 +0530

    Limit concurrency of records validation (#800)

--- functions/src/FidelityCheck/FC.functions.ts
+++ functions/src/FidelityCheck/FC.functions.ts
@@ -63,136 +63,142 @@ export const validateRecords = functions
       'Fidelity Checked without topics',
     ]);
 
-    const spreadsheetStatuses = await pMap(rows, async (row, index) => {
-      // Normalizing input data: fixing humans’ mistypings
-      STRING_COLUMNS.forEach((key) => {
-        row[key] = row[key]?.toString()?.trim() || null;
-      });
-      BOOLEAN_COLUMNS.forEach((key) => {
-        row[key] = row[key] || false;
-      });
-
-      // Checking for sanity of Archive ID
-      if (!Number.isFinite(row['Archive ID'])) return 'No Archive ID.';
-
-      if (rows.findIndex((x) => x['Archive ID'] === row['Archive ID']) < index)
-        return 'Duplicate Archive ID.';
-
-      const recordSnapshot = snapshot.child(row['Archive ID'].toString());
-      const existingRecord = recordSnapshot.val() as FidelityCheckRecord;
-
-      // Removing the approval from the database if Ready For Archive is `false`
-      // Doing this early ensures unpublishing of a file even if there are validation issues
-      if (row['Ready For Archive'] === false && existingRecord?.approval) {
-        functions.logger.info('Removing approval from', row['Archive ID']);
-        await recordSnapshot.ref.update({
-          approval: null,
+    const spreadsheetStatuses = await pMap(
+      rows,
+      async (row, index) => {
+        // Normalizing input data: fixing humans’ mistypings
+        STRING_COLUMNS.forEach((key) => {
+          row[key] = row[key]?.toString()?.trim() || null;
         });
-      }
-
-      // Basic row validation
-      const result = validator.validate(row, index, rows);
-      if (!result.isValid) return result.messages.join('\n');
-
-      if (
-        row['Fidelity Checked'] !== true &&
-        row['Fidelity Checked without topics'] !== true
-      ) {
-        // Removing the fidelity check from the database
-        await recordSnapshot.ref.update({
-          fidelityCheck: null,
+        BOOLEAN_COLUMNS.forEach((key) => {
+          row[key] = row[key] || false;
         });
 
-        return 'Awaiting FC.';
-      }
+        // Checking for sanity of Archive ID
+        if (!Number.isFinite(row['Archive ID'])) return 'No Archive ID.';
 
-      // General fidelity check supercedes the quick one (without topics)
-      const fidelityCheckDate = row['Fidelity Checked']
-        ? row['FC Date']
-        : row['FC Date without topics'];
+        if (
+          rows.findIndex((x) => x['Archive ID'] === row['Archive ID']) < index
+        )
+          return 'Duplicate Archive ID.';
 
-      const file = await StorageManager.getMostRecentFile(
-        StorageManager.getCandidateFiles(row['Task ID'])
-      );
-      if (!file) return 'File not found';
+        const recordSnapshot = snapshot.child(row['Archive ID'].toString());
+        const existingRecord = recordSnapshot.val() as FidelityCheckRecord;
 
-      const fileCreationTime = DateTime.fromISO(
-        file.metadata.timeCreated // ISO format, e.g. 2020-08-24T09:28:12.483Z
-      );
+        // Removing the approval from the database if Ready For Archive is `false`
+        // Doing this early ensures unpublishing of a file even if there are validation issues
+        if (row['Ready For Archive'] === false && existingRecord?.approval) {
+          functions.logger.info('Removing approval from', row['Archive ID']);
+          await recordSnapshot.ref.update({
+            approval: null,
+          });
+        }
 
-      const fidelityCheckTime = DateTimeConverter.fromSerialDate(
-        fidelityCheckDate,
-        sheet.timeZone
-      );
+        // Basic row validation
+        const result = validator.validate(row, index, rows);
+        if (!result.isValid) return result.messages.join('\n');
 
-      /**
-       * If the date is midnight, it means that the date was entered manually during this day.
-       * Hence, using the end of that day as an “exact” FC time.
-       */
-      const exactFidelityCheckTime =
-        fidelityCheckTime === fidelityCheckTime.startOf('day')
-          ? fidelityCheckTime.endOf('day')
-          : fidelityCheckTime;
+        if (
+          row['Fidelity Checked'] !== true &&
+          row['Fidelity Checked without topics'] !== true
+        ) {
+          // Removing the fidelity check from the database
+          await recordSnapshot.ref.update({
+            fidelityCheck: null,
+          });
 
-      // The FC Date should be later than the time when the file was created.
-      if (fileCreationTime > exactFidelityCheckTime)
-        return `File was created on ${fileCreationTime.toISODate()}, after Fidelity Check on ${exactFidelityCheckTime.toISODate()}.`;
+          return 'Awaiting FC.';
+        }
 
-      const fileReference: StorageFileReference = {
-        bucket: file.bucket.name,
-        name: file.name,
-        generation: file.metadata.generation,
-      };
-      const fidelityCheck: FidelityCheck = {
-        timestamp: exactFidelityCheckTime.toMillis(),
-        author: row['FC Initials'],
-      };
+        // General fidelity check supercedes the quick one (without topics)
+        const fidelityCheckDate = row['Fidelity Checked']
+          ? row['FC Date']
+          : row['FC Date without topics'];
 
-      if (
-        // Using -Infinity to make sure that in absense of existing record the new record will be considered newer
-        fidelityCheck.timestamp <=
-        (existingRecord?.fidelityCheck?.timestamp || -Infinity)
-      ) {
-        // Comparing file info if the FC Date was not bumped
-        if (getDiff(existingRecord.file, fileReference).length)
-          return 'File was updated since last fidelity check.';
-      }
-      // Updating the database if the FC Date was bumped
-      else
-        await recordSnapshot.ref.update({
-          file: fileReference,
-          fidelityCheck,
-        });
+        const file = await StorageManager.getMostRecentFile(
+          StorageManager.getCandidateFiles(row['Task ID'])
+        );
+        if (!file) return 'File not found';
 
-      if (row['Ready For Archive'] !== true)
-        return 'Awaiting Ready For Archive.';
+        const fileCreationTime = DateTime.fromISO(
+          file.metadata.timeCreated // ISO format, e.g. 2020-08-24T09:28:12.483Z
+        );
 
-      const approval: Approval = {
-        timestamp: DateTimeConverter.fromSerialDate(
-          row['Finalization Date'],
+        const fidelityCheckTime = DateTimeConverter.fromSerialDate(
+          fidelityCheckDate,
           sheet.timeZone
-        ).toMillis(),
-        topicsReady: row['Topics Ready'],
-      };
+        );
 
-      const contentDetails: ContentDetails = {
-        title: row['Suggested Title'],
-        topics: row.Topics,
-        date: row['Date (yyyymmdd format)'],
-        dateUncertain: row['Date uncertain'],
-        timeOfDay: row['AM/PM'],
-        location: row.Location,
-        locationUncertain: row['Location uncertain'],
-        category: row.Category,
-        languages: row['Lecture Language'],
-        percentage: row['Srila Gurudeva Timing'],
-        otherSpeaker: row['Other Guru-varga'],
-        seriesInputs: row['Series/Sastra Inputs'],
-        soundQualityRating: row['Sound Rating'],
-      };
+        /**
+         * If the date is midnight, it means that the date was entered manually during this day.
+         * Hence, using the end of that day as an “exact” FC time.
+         */
+        const exactFidelityCheckTime =
+          fidelityCheckTime === fidelityCheckTime.startOf('day')
+            ? fidelityCheckTime.endOf('day')
+            : fidelityCheckTime;
 
-      const backMapping: Record<keyof ContentDetails, keyof FidelityCheckRow> =
-        {
+        // The FC Date should be later than the time when the file was created.
+        if (fileCreationTime > exactFidelityCheckTime)
+          return `File was created on ${fileCreationTime.toISODate()}, after Fidelity Check on ${exactFidelityCheckTime.toISODate()}.`;
+
+        const fileReference: StorageFileReference = {
+          bucket: file.bucket.name,
+          name: file.name,
+          generation: file.metadata.generation,
+        };
+        const fidelityCheck: FidelityCheck = {
+          timestamp: exactFidelityCheckTime.toMillis(),
+          author: row['FC Initials'],
+        };
+
+        if (
+          // Using -Infinity to make sure that in absense of existing record the new record will be considered newer
+          fidelityCheck.timestamp <=
+          (existingRecord?.fidelityCheck?.timestamp || -Infinity)
+        ) {
+          // Comparing file info if the FC Date was not bumped
+          if (getDiff(existingRecord.file, fileReference).length)
+            return 'File was updated since last fidelity check.';
+        }
+        // Updating the database if the FC Date was bumped
+        else
+          await recordSnapshot.ref.update({
+            file: fileReference,
+            fidelityCheck,
+          });
+
+        if (row['Ready For Archive'] !== true)
+          return 'Awaiting Ready For Archive.';
+
+        const approval: Approval = {
+          timestamp: DateTimeConverter.fromSerialDate(
+            row['Finalization Date'],
+            sheet.timeZone
+          ).toMillis(),
+          topicsReady: row['Topics Ready'],
+        };
+
+        const contentDetails: ContentDetails = {
+          title: row['Suggested Title'],
+          topics: row.Topics,
+          date: row['Date (yyyymmdd format)'],
+          dateUncertain: row['Date uncertain'],
+          timeOfDay: row['AM/PM'],
+          location: row.Location,
+          locationUncertain: row['Location uncertain'],
+          category: row.Category,
+          languages: row['Lecture Language'],
+          percentage: row['Srila Gurudeva Timing'],
+          otherSpeaker: row['Other Guru-varga'],
+          seriesInputs: row['Series/Sastra Inputs'],
+          soundQualityRating: row['Sound Rating'],
+        };
+
+        const backMapping: Record<
+          keyof ContentDetails,
+          keyof FidelityCheckRow
+        > = {
           title: 'Suggested Title',
           topics: 'Topics',
           date: 'Date (yyyymmdd format)',
@@ -208,33 +214,39 @@ export const validateRecords = functions
           soundQualityRating: 'Sound Rating',
         };
 
-      if (
-        // Using -Infinity to make sure that in absense of existing record the new record will be considered newer
-        approval.timestamp <= (existingRecord?.approval?.timestamp || -Infinity)
-      ) {
-        const changedColumns = getDiff(
-          existingRecord.contentDetails,
-          contentDetails,
-          true // keep old values
-        )
-          .filter(
-            (d) =>
-              // Ignore topics changes if they were not approved earlier
-              (existingRecord.approval?.topicsReady ||
-                d.path[0] !== 'topics') &&
-              // Absent value from Firebase is undefined, but `null` in the spreadsheet
-              !(d.op === 'add' && d.val === null)
+        if (
+          // Using -Infinity to make sure that in absense of existing record the new record will be considered newer
+          approval.timestamp <=
+          (existingRecord?.approval?.timestamp || -Infinity)
+        ) {
+          const changedColumns = getDiff(
+            existingRecord.contentDetails,
+            contentDetails,
+            true // keep old values
           )
-          .map((d) => backMapping[d.path[0]]);
+            .filter(
+              (d) =>
+                // Ignore topics changes if they were not approved earlier
+                (existingRecord.approval?.topicsReady ||
+                  d.path[0] !== 'topics') &&
+                // Absent value from Firebase is undefined, but `null` in the spreadsheet
+                !(d.op === 'add' && d.val === null)
+            )
+            .map((d) => backMapping[d.path[0]]);
 
-        if (changedColumns.length)
-          return `Changed after finalization: ${changedColumns.join(', ')}.`;
+          if (changedColumns.length)
+            return `Changed after finalization: ${changedColumns.join(', ')}.`;
+        }
+        // Updating the database if the Finalization Date was bumped
+        else await recordSnapshot.ref.update({ approval, contentDetails });
+
+        return 'OK';
+      },
+      {
+        // More than 5000 causes TeenyStatisticsWarning: Possible excessive concurrent requests detected.
+        concurrency: 1000,
       }
-      // Updating the database if the Finalization Date was bumped
-      else await recordSnapshot.ref.update({ approval, contentDetails });
-
-      return 'OK';
-    });
+    );
 
     await sheet.updateColumn('Validation Status', spreadsheetStatuses);
   });
