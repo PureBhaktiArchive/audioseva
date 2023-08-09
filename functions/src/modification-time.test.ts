bc59446d225f4b0df81d3c93ad8cc151b90ac88c
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-08-09 15:09:45 +0530

    Add more tests and fix the code

--- functions/src/modification-time.test.ts
+++ functions/src/modification-time.test.ts
@@ -9,29 +9,27 @@ describe('File modification time', () => {
   const sampleTimeCreated = '2020-08-23T14:02:54.724Z';
   const sampleTimeCreatedTimestamp = 1598191374724;
 
-  test('should be taken from timeCreated if no mtime', () => {
+  // Testing mtime that is before and after timeCreated
+  test.each`
+    mtime              | result
+    ${'1598189082'}    | ${1598189082000}
+    ${'1598289082'}    | ${1598289082000}
+    ${'1598189082000'} | ${sampleTimeCreatedTimestamp}
+    ${'something'}     | ${sampleTimeCreatedTimestamp}
+    ${'123'}           | ${sampleTimeCreatedTimestamp}
+    ${''}              | ${sampleTimeCreatedTimestamp}
+    ${null}            | ${sampleTimeCreatedTimestamp}
+    ${undefined}       | ${sampleTimeCreatedTimestamp}
+  `('with mtime $mtime', ({ mtime, result }) => {
     const file = {
       metadata: {
         timeCreated: sampleTimeCreated,
+        metadata: undefined,
       },
     };
-    expect(modificationTime(file as File).toMillis()).toEqual(
-      sampleTimeCreatedTimestamp
-    );
-  });
+    if (mtime !== undefined)
+      file.metadata.metadata = { ['goog-reserved-file-mtime']: mtime };
 
-  // Testing mtime that is before and after timeCreated
-  test.each`
-    timeCreated                   | mtime
-    ${'2020-08-23T14:02:54.724Z'} | ${1598189082}
-    ${'2020-08-23T14:02:54.724Z'} | ${1598289082}
-  `('should be taken from mtime $mtime', ({ timeCreted, mtime }) => {
-    const file = {
-      metadata: {
-        timeCreted,
-        metadata: { ['goog-reserved-file-mtime']: mtime },
-      },
-    };
-    expect(modificationTime(file as File).toSeconds()).toEqual(mtime);
+    expect(modificationTime(file as File).toMillis()).toEqual(result);
   });
 });
--- functions/src/modification-time.ts
+++ functions/src/modification-time.ts
@@ -12,10 +12,16 @@ import { DateTime } from 'luxon';
  * @returns File's modification time in epoch seconds
  */
 export const modificationTime = (file: File): DateTime => {
-  const mtime = file.metadata?.metadata?.['goog-reserved-file-mtime'];
-  return Number.isFinite(mtime)
+  /**
+   * All metadata items are strings.
+   * Casting type until `@google-cloud/storage` v7, where better typing of metadata is introduced
+   * https://github.com/googleapis/nodejs-storage/pull/2234
+   */
+  const mtime = file.metadata.metadata?.['goog-reserved-file-mtime'] as string;
+  // Only 10-digit numbers are considered valid (seva started in 2017)
+  return /^[0-9]{10}$/.test(mtime)
     ? // This metadata contains time in Unix epoch seconds
-      DateTime.fromSeconds(mtime)
-    : // ISO format, e.g. 2020-08-24T09:28:12.483Z
+      DateTime.fromSeconds(+mtime)
+    : // RFC 3339 (in fact ISO), e.g. 2020-08-24T09:28:12.483Z - see https://cloud.google.com/storage/docs/json_api/v1/objects#resource
       DateTime.fromISO(file.metadata.timeCreated);
 };
