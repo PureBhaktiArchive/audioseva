508676fe1590a4c1dc5b833f2dc342c620546bb0
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-10-11 22:26:56 +0530

    Debug message

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  configureWebpack: {
-    output: {
-      // https://github.com/webpack/webpack/issues/14532
-      // https://stackoverflow.com/a/73465262/3082178
-      hashFunction: 'md5',
-    },
+  configureWebpack: (config) => {
+    console.log('Configuring Webpack');
+    // https://github.com/webpack/webpack/issues/14532
+    // https://stackoverflow.com/a/73465262/3082178
+    config.output.hashFunction = 'md5';
   },
   chainWebpack: (config) => {
     config.plugins.delete('prefetch');
