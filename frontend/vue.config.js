5d23b2cb97849f8ce5017d9a5779069dba9e1804
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-10-12 13:17:15 +0530

    Debug

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,26 @@
+const crypto = require('crypto');
+
+/**
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash('md4');
+} catch (e) {
+  console.warn('Crypto "MD4" is not supported anymore by this Node.js version');
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    console.log(`Creating an ${alg} hash`);
+    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+  };
+}
+
 module.exports = {
   configureWebpack: (config) => {
-    console.log('Configuring Webpack');
+    console.log('Original hash function is', config.output.hashFunction);
     // https://github.com/webpack/webpack/issues/14532
     // https://stackoverflow.com/a/73465262/3082178
-    config.output.hashFunction = 'md5';
+    // config.output.hashFunction = 'md5';
   },
   chainWebpack: (config) => {
     config.plugins.delete('prefetch');
