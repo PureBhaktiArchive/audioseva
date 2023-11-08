59281811817686a85e239e2d269eba0c0c242efa
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2023-11-08 11:18:34 +0700

    Clarify comments to the assignment-only record

--- functions/src/FidelityCheck/FinalRecord.ts
+++ functions/src/FidelityCheck/FinalRecord.ts
@@ -7,13 +7,14 @@ import { ContentDetails } from './ContentDetails';
 
 export type FinalRecord = AssignmentRecord | NormalRecord | RedirectRecord;
 
+// Only task ID is preserved when a source record is missing or unpublished
 export interface AssignmentRecord {
   taskId: string;
 }
 
 export interface NormalRecord extends AssignmentRecord {
   file: StorageFileReference;
-  contentDetails: ContentDetails; // Absent when a record is unpublished
+  contentDetails: ContentDetails;
 }
 
 export interface RedirectRecord {
