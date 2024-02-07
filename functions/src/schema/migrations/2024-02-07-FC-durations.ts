fa0f7079399327024a5a4c53c32b876cdf445d8d
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-02-07 15:59:34 +0700

    Add a migration for filling durations in FC

---
+++ functions/src/schema/migrations/2024-02-07-FC-durations.ts
@@ -0,0 +1,30 @@
+import { getDatabase } from 'firebase-admin/database';
+import { filter, map, pipe } from 'iter-ops';
+import { FidelityCheckRecord } from '../../FidelityCheck/FidelityCheckRecord';
+import { objectToIterableEntries } from '../../iterable-helpers';
+import { getFileDurationPath, metadataCacheRef } from '../../metadata-database';
+
+export const fillFidelityCheckDurations = async () => {
+  const fidelityRef = getDatabase().ref('/FC/records');
+  const [recordsSnapshot, metadataCacheSnapshot] = await Promise.all([
+    fidelityRef.once('value'),
+    metadataCacheRef.once('value'),
+  ]);
+
+  await fidelityRef.update(
+    Object.fromEntries(
+      pipe(
+        objectToIterableEntries(
+          recordsSnapshot.val() as Record<string, FidelityCheckRecord>
+        ),
+        filter(([, record]) => !!record.file && !record.duration),
+        map(([taskId, record]) => [
+          `${taskId}/duration`,
+          metadataCacheSnapshot
+            .child(getFileDurationPath(record.file))
+            .val() as number,
+        ])
+      )
+    )
+  );
+};
