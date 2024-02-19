PureBhaktiArchive/audioseva

commit 56057db35a8e5015a874e0bd250e8b3f3991e9ba

Update dependency typescript to v4.9.5 (#865)
Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
Co-authored-by: Ananta Krsna dasa <anantakrishna.kem@gmail.com>

functions/src/AbstractRepository.ts
@@ -3,19 +3,21 @@
  */
 
 import { database } from 'firebase-admin';
-import { createSchema, morphism, StrictSchema } from 'morphism';
+import { DateTime } from 'luxon';
 import { Allotment, AllotmentStatus, allotmentValidator } from './Allotment';
 import { AllotmentRow } from './AllotmentRow';
 import { DateTimeConverter } from './DateTimeConverter';
-import { flatten } from './flatten';
 import { RequireOnly } from './RequireOnly';
 import { Spreadsheet } from './Spreadsheet';
+import { flatten } from './flatten';
 import _ = require('lodash');
+
+type BaseTask<TId extends string> = Allotment & Record<TId, string>;
 
 export abstract class AbstractRepository<
   TRow extends AllotmentRow,
-  TTask extends Allotment & { [id in TId]: string },
-  TId extends keyof TTask
+  TTask extends BaseTask<TId>,
+  TId extends keyof TTask & string
 > {
   constructor(
     private readonly spreadsheetId: string,
@@ -36,25 +38,55 @@ export abstract class AbstractRepository<
     return (await this.allotmentsSheet()).getRows();
   }
 
-  protected mapFromRows: (
-    rows: TRow[]
-  ) => Pick<TTask, TId | keyof Allotment>[] = morphism(
-    createSchema<Pick<TTask, TId | keyof Allotment>, TRow>({
-      [this.idPropertyName]: this.idColumnName,
-      status: ({ Status: status }) =>
-        (status?.trim() as AllotmentStatus) || AllotmentStatus.Spare,
-      timestampGiven: ({ 'Date Given': date }) =>
-        date ? DateTimeConverter.fromSerialDate(date).toMillis() : null,
-      timestampDone: ({ 'Date Done': date }) =>
-        date ? DateTimeConverter.fromSerialDate(date).toMillis() : null,
-      assignee: ({ Devotee: name, Email: emailAddress }) => ({
-        name: name?.trim() || null,
-        emailAddress: emailAddress?.trim() || null,
-      }),
-    } as StrictSchema<Pick<TTask, TId | keyof Allotment>, TRow>)
-  );
+  protected mapFromRows = (rows: TRow[]): BaseTask<TId>[] =>
+    rows.map(
+      ({
+        Status: status,
+        'Date Given': dateGiven,
+        'Date Done': dateDone,
+        Devotee: name,
+        Email: emailAddress,
+      }) =>
+        ({
+          [this.idPropertyName]: this.idColumnName,
+          status: (status?.trim() as AllotmentStatus) || AllotmentStatus.Spare,
+          timestampGiven: dateGiven
+            ? DateTimeConverter.fromSerialDate(dateGiven).toMillis()
+            : null,
+          timestampDone: dateDone
+            ? DateTimeConverter.fromSerialDate(dateDone).toMillis()
+            : null,
+          assignee: {
+            name: name?.trim() || null,
+            emailAddress: emailAddress?.trim() || null,
+          },
+        } as BaseTask<TId>)
+    );
 
-  protected abstract mapToRows(tasks: TTask[]): TRow[];
+  protected mapAllotment = (task: TTask): AllotmentRow => ({
+    [this.idColumnName]: task[this.idPropertyName],
+    Status:
+      task.status === undefined
+        ? undefined
+        : task.status === AllotmentStatus.Spare
+        ? null
+        : task.status,
+    'Date Given': task.timestampGiven
+      ? DateTimeConverter.toSerialDate(DateTime.fromMillis(task.timestampGiven))
+      : null,
+    'Date Done': task.timestampDone
+      ? DateTimeConverter.toSerialDate(DateTime.fromMillis(task.timestampDone))
+      : null,
+    Devotee: task.assignee?.name || null,
+    Email: task.assignee?.emailAddress || null,
+  });
+
+  protected abstract mapTask(task: TTask): TRow;
+  protected mapToRows = (tasks: TTask[]): TRow[] =>
+    tasks.map((task) => ({
+      ...this.mapAllotment(task),
+      ...this.mapTask(task),
+    }));
 
   protected getTaskRef = (id: string) => this.allotmentsRef.child(id);
 
@@ -75,7 +107,7 @@ export abstract class AbstractRepository<
     await this.saveToDatabase(tasks);
 
     const updatedTasks = await this.getTasks(
-      _.map(tasks, (task) => _.get(task, this.idPropertyName))
+      tasks.map((task) => task[this.idPropertyName])
     );
 
     await this.saveToSpreadsheet(updatedTasks);
@@ -132,7 +164,7 @@ export abstract class AbstractRepository<
     /// Adding missing tasks from the database to the spreadsheet
 
     const idsInSpreadsheet = new Set(
-      _.map(tasksFromSpreadsheet, (task) => _.get(task, this.idPropertyName))
+      tasksFromSpreadsheet.map((task) => task[this.idPropertyName])
     );
 
     const tasksForSpreadsheet = _.chain(tasksFromDatabase)
@@ -148,8 +180,8 @@ export abstract class AbstractRepository<
 
     /// Updating allotment info from the spreadsheet to the database
 
-    const tasksForDatabase = _.chain(tasksFromSpreadsheet)
-      .filter((taskFromSpreadsheet) => {
+    const tasksForDatabase = tasksFromSpreadsheet.filter(
+      (taskFromSpreadsheet) => {
         const id = taskFromSpreadsheet[this.idPropertyName];
 
         if (id.match(/[.#$/[\]]/)) {
@@ -192,10 +224,11 @@ export abstract class AbstractRepository<
         );
 
         return true;
-      })
+      }
       // Type casting is required to pass this successfully to `saveToDatabase`.
       // Asked question https://stackoverflow.com/q/63216805/3082178
-      .value() as RequireOnly<TTask, TId>[];
+      // It is also discussed in https://stackoverflow.com/q/56788853/3082178
+    ) as RequireOnly<TTask, TId>[];
 
     if (dryRun) console.log(`DRY RUN, doing nothing.`);
     else {

functions/src/SoundQualityReporting/TasksRepository.ts
@@ -3,11 +3,8 @@
  */
 
 import * as functions from 'firebase-functions';
-import { DateTime } from 'luxon';
-import { createSchema, morphism } from 'morphism';
 import { AbstractRepository } from '../AbstractRepository';
-import { AllotmentStatus, isActiveAllotment } from '../Allotment';
-import { DateTimeConverter } from '../DateTimeConverter';
+import { isActiveAllotment } from '../Allotment';
 import { ReportingAllotmentRow } from '../ReportingAllotmentRow';
 import { ReportingTask } from '../ReportingTask';
 import { SpareFile } from './SpareFile';
@@ -31,28 +28,9 @@ export class TasksRepository extends AbstractRepository<
     );
   }
 
-  protected mapToRows = morphism(
-    createSchema<ReportingAllotmentRow, ReportingTask>({
-      'File Name': 'fileName',
-      Status: ({ status }) =>
-        status === undefined
-          ? undefined
-          : status === AllotmentStatus.Spare
-          ? null
-          : status,
-      'Date Given': ({ timestampGiven }) =>
-        timestampGiven
-          ? DateTimeConverter.toSerialDate(DateTime.fromMillis(timestampGiven))
-          : null,
-      'Date Done': ({ timestampDone }) =>
-        timestampDone
-          ? DateTimeConverter.toSerialDate(DateTime.fromMillis(timestampDone))
-          : null,
-      Devotee: 'assignee.name',
-      Email: 'assignee.emailAddress',
-      Notes: 'notes',
-    })
-  );
+  protected mapTask = (task: ReportingTask): ReportingAllotmentRow => ({
+    'File Name': task.fileName,
+  });
 
   public async getLists() {
     return _(await this.getRows())

functions/src/TrackEditing/TasksRepository.ts
@@ -5,7 +5,6 @@
 import * as functions from 'firebase-functions';
 import { DateTime } from 'luxon';
 import { AbstractRepository } from '../AbstractRepository';
-import { AllotmentStatus } from '../Allotment';
 import { AudioChunk } from '../AudioChunk';
 import { DateTimeConverter } from '../DateTimeConverter';
 import { FileVersion } from '../FileVersion';
@@ -30,63 +29,43 @@ export class TasksRepository extends AbstractRepository<
     super(functions.config().te.spreadsheet.id, 'id', 'Task ID', tasksRef);
   }
 
-  protected mapToRows(tasks: TrackEditingTask[]): TrackEditingAllotmentRow[] {
-    return tasks.map((task) => {
-      const lastVersionKey = _.findLastKey(task.versions);
-      const lastResolvedVersion = _.findLast(
-        task.versions,
-        (version) => !!version.resolution
-      );
+  protected mapTask = (task: TrackEditingTask): TrackEditingAllotmentRow => {
+    const lastVersionKey = _.findLastKey(task.versions);
+    const lastResolvedVersion = _.findLast(
+      task.versions,
+      (version) => !!version.resolution
+    );
 
-      return {
-        'Task ID': task.id,
-        'SEd?': task.isRestored ? 'SEd' : 'non-SEd',
-        Status:
-          task.status === undefined
-            ? undefined
-            : task.status === AllotmentStatus.Spare
-            ? null
-            : task.status,
-        'Date Given': task.timestampGiven
+    return {
+      'Task ID': task.id,
+      'SEd?': task.isRestored ? 'SEd' : 'non-SEd',
+      'Upload Link': lastVersionKey
+        ? trackEditingVersionOutputLink(task.id, lastVersionKey)
+        : null,
+      'Upload Date':
+        lastVersionKey && task.versions[lastVersionKey]?.timestamp
           ? DateTimeConverter.toSerialDate(
-              DateTime.fromMillis(task.timestampGiven)
+              DateTime.fromMillis(task.versions[lastVersionKey].timestamp)
             )
           : null,
-        'Date Done': task.timestampDone
-          ? DateTimeConverter.toSerialDate(
-              DateTime.fromMillis(task.timestampDone)
-            )
-          : null,
-        Devotee: task.assignee?.name || null,
-        Email: task.assignee?.emailAddress || null,
-        'Upload Link': lastVersionKey
-          ? trackEditingVersionOutputLink(task.id, lastVersionKey)
-          : null,
-        'Upload Date':
-          lastVersionKey && task.versions[lastVersionKey]?.timestamp
-            ? DateTimeConverter.toSerialDate(
-                DateTime.fromMillis(task.versions[lastVersionKey].timestamp)
-              )
-            : null,
-        'Uploaded By': lastVersionKey
-          ? task.versions[lastVersionKey]?.author?.name
-          : null,
-        'Latest Resolution': lastResolvedVersion
-          ? lastResolvedVersion.resolution.isApproved
-            ? 'Approved'
-            : `Disapproved: ${lastResolvedVersion.resolution.feedback}`
-          : null,
-        'Resolution Date': lastResolvedVersion
-          ? DateTimeConverter.toSerialDate(
-              DateTime.fromMillis(lastResolvedVersion.resolution.timestamp)
-            )
-          : null,
-        'Checked By': lastResolvedVersion
-          ? lastResolvedVersion.resolution.author?.name || null
-          : null,
-      };
-    });
-  }
+      'Uploaded By': lastVersionKey
+        ? task.versions[lastVersionKey]?.author?.name
+        : null,
+      'Latest Resolution': lastResolvedVersion
+        ? lastResolvedVersion.resolution.isApproved
+          ? 'Approved'
+          : `Disapproved: ${lastResolvedVersion.resolution.feedback}`
+        : null,
+      'Resolution Date': lastResolvedVersion
+        ? DateTimeConverter.toSerialDate(
+            DateTime.fromMillis(lastResolvedVersion.resolution.timestamp)
+          )
+        : null,
+      'Checked By': lastResolvedVersion
+        ? lastResolvedVersion.resolution.author?.name || null
+        : null,
+    };
+  };
 
   public getNewVersionRef(taskId: string) {
     return this.getTaskRef(taskId).child('versions').push();

frontend/src/views/TE/Allotment.vue
@@ -215,7 +215,9 @@ export default class Allotment extends Mixins<TaskMixin>(TaskMixin) {
       );
       this.submissionStatus = 'complete';
     } catch (error) {
-      alert(error.message);
+      if (error instanceof Error) {
+        alert(error.message);
+      }
       this.submissionStatus = 'error';
     }
   }

frontend/src/views/TE/Task.vue
@@ -303,7 +303,7 @@ export default class Task extends Mixins<TaskMixin, FormatTime>(
       this.task = _.merge({}, this.task, this.cancelData());
     } catch (e) {
       // eslint-disable-next-line no-console
-      console.error(e.message);
+      console.error(e);
     }
   }
 

functions/src/RequireOnly.ts
@@ -2,5 +2,4 @@
  * sri sri guru gauranga jayatah
  */
 
-export type RequireOnly<T, K extends keyof T> = Partial<T> &
-  Required<Pick<T, K>>;
+export type RequireOnly<T, K extends keyof T> = Partial<T> & Pick<T, K>;

commit 7ee15671483169e4031680089ea840095f0b00f8

Fix `syncAllotments` (#917)
A fix to a bug introduced in 56057db35a8e5015a874e0bd250e8b3f3991e9ba (#865).

functions/src/AbstractRepository.ts
@@ -18,11 +18,12 @@ export abstract class AbstractRepository<
   TRow extends AllotmentRow,
   TTask extends BaseTask<TId>,
   TId extends keyof TTask & string,
+  TIdColumn extends keyof TRow & string,
 > {
   constructor(
     private readonly spreadsheetId: string,
     private readonly idPropertyName: TId,
-    private readonly idColumnName: string,
+    private readonly idColumnName: TIdColumn,
     private readonly allotmentsRef: database.Reference
   ) {}
 
@@ -41,6 +42,7 @@ export abstract class AbstractRepository<
   protected mapFromRows = (rows: TRow[]): BaseTask<TId>[] =>
     rows.map(
       ({
+        [this.idColumnName]: id,
         Status: status,
         'Date Given': dateGiven,
         'Date Done': dateDone,
@@ -48,7 +50,7 @@ export abstract class AbstractRepository<
         Email: emailAddress,
       }) =>
         ({
-          [this.idPropertyName]: this.idColumnName,
+          [this.idPropertyName]: id,
           status: (status?.trim() as AllotmentStatus) || AllotmentStatus.Spare,
           timestampGiven: dateGiven
             ? DateTimeConverter.fromSerialDate(dateGiven).toMillis()

functions/src/SoundQualityReporting/TasksRepository.ts
@@ -17,7 +17,8 @@ const allotmentsRef = baseRef.child(`allotments`);
 export class TasksRepository extends AbstractRepository<
   ReportingAllotmentRow,
   ReportingTask,
-  'fileName'
+  'fileName',
+  'File Name'
 > {
   constructor() {
     super(

functions/src/TrackEditing/TasksRepository.ts
@@ -23,7 +23,8 @@ const tasksRef = baseRef.child(`tasks`);
 export class TasksRepository extends AbstractRepository<
   TrackEditingAllotmentRow,
   TrackEditingTask,
-  'id'
+  'id',
+  'Task ID'
 > {
   constructor() {
     super(