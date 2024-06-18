481f540f08e38f737dc51e365c137984e787253a
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-06-18 12:59:19 +0700

    Add PrimeVue

--- TR/src/main.js
+++ TR/src/main.js
@@ -1,5 +1,13 @@
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
 import App from './App.vue';
 
 const app = createApp(App);
+// @ts-expect-error -- As of now there is a type mismatch https://github.com/orgs/primefaces/discussions/2160
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
 app.mount('#app');
