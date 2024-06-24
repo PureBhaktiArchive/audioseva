87b3361c14e0dc0af8f5e8d04502684c1fd63f47
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-06-24 10:45:36 +0700

    Use local emulator for functions in development

--- TR/src/firebase.js
+++ TR/src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, getRedirectResult } from 'firebase/auth';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
 
 export const firebaseApp = initializeApp({
   ...JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG),
@@ -8,5 +9,13 @@ export const firebaseApp = initializeApp({
   authDomain: window.location.host,
 });
 
+// Instrumenting the firebase app to use a local emulator for functions
+// https://firebase.google.com/docs/functions/local-emulator#instrument-functions
+if (import.meta.env.DEV)
+  connectFunctionsEmulator(
+    getFunctions(firebaseApp),
+    import.meta.env.VITE_FUNCTIONS_HOST || 'localhost',
+    import.meta.env.VITE_FUNCTIONS_PORT || 5001
+  );
 // Running this each time because who knows if there was a redirect.
 getRedirectResult(getAuth());
