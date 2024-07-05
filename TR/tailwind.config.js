a6a469875b85e862df08f24a35c2a0d32ea28637
Ananta Krsna dasa <anantakrishna.kem@gmail.com>
2024-07-05 10:53:19 +0700

    Add Iconify

--- TR/tailwind.config.js
+++ TR/tailwind.config.js
@@ -1,8 +1,10 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()],
 };
